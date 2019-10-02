8cf19cb04b62cda19ebba24f3fd6416b33ef62cd
Suzanne Rozier <suz@truss.works>
2019-10-02 15:08:03 -0400

    Setup webpack, library export, jest

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Alert from './Alert/Alert'
+
+export {
+  Alert,
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'truss-uswds-react.js',
+    library: 'TrussUSWDSReact',
+    libraryTarget: 'umd',
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
+  },
+  resolve: {
+    extensions: ['.jsx', 'js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
