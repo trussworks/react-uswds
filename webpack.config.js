87c177550668a22758bcb41e3d9570fe2486b248
Suzanne Rozier <suz@truss.works>
2019-10-07 11:12:35 -0400

    Convert index.js to TS

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'react-uswds.js',
