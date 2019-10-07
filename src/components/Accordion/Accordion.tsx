00ea6e1d74c9837cdaf7aa97e52a18f4ffaf2ed6
Suzanne Rozier <suz@truss.works>
2019-10-07 16:44:46 -0400

    Write Accordion component with hooks!

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import classnames from 'classnames'
+
+interface AccordionItem {
+  title: React.ReactNode | string
+  content: React.ReactNode
+  expanded: boolean
+  id: string
+  handleToggle?: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+interface AccordionProps {
+  bordered: boolean
+  items: AccordionItem[]
+}
+
+export const AccordionItem = (props: AccordionItem): React.ReactElement => {
+  const { title, id, content, expanded, handleToggle } = props
+
+  return (
+    <>
+      <h2 className="usa-accordion__heading">
+        <button
+          type="button"
+          className="usa-accordion__button"
+          aria-expanded={expanded}
+          aria-controls={id}
+          onClick={handleToggle}>
+          {title}
+        </button>
+      </h2>
+      <div
+        id={id}
+        className="usa-accordion__content usa-prose"
+        hidden={!expanded}>
+        {content}
+      </div>
+    </>
+  )
+}
+
+export const Accordion = (props: AccordionProps): React.ReactElement => {
+  const { bordered, items } = props
+
+  const [openItem, setOpenState] = useState(items[0].id)
+
+  const classes = classnames('usa-accordion', {
+    'usa-accordion--bordered': bordered,
+  })
+
+  return (
+    <div className={classes}>
+      {items.map((item, i) => (
+        <AccordionItem
+          key={`accordionItem_${i}`}
+          {...item}
+          expanded={openItem === item.id}
+          handleToggle={(): void => {
+            setOpenState(item.id)
+          }}
+        />
+      ))}
+    </div>
+  )
+}
