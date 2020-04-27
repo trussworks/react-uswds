9d1de2d24226a836fad38bced1d6ac9035702e43
eamahanna <emahanna@truss.works>
2020-04-27 07:41:21 -0800

    Update to use components

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,37 +1,35 @@
 import React from 'react'
 import classnames from 'classnames'
 
+import { Button } from '../Button/Button'
+import { Form } from '../forms/Form/Form'
+import { Label } from '../forms/Label/Label'
+import { TextInput } from '../forms/TextInput/TextInput'
+
 interface SearchInputProps {
-  onSubmit?: (event: React.FormEvent<HTMLFormElement>) => void
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void
   className?: string
 }
 
-export const SearchInput = (
-  props: SearchInputProps & React.FormHTMLAttributes<HTMLFormElement>
-): React.ReactElement => {
-  const { onSubmit, className, ...formProps } = props
+export const SearchInput = (props: SearchInputProps): React.ReactElement => {
+  const { onSubmit, className } = props
   const classes = classnames('usa-search usa-search--small', className)
 
   return (
-    <form
-      data-testid="searchInput"
-      className={classes}
-      role="search"
-      onSubmit={onSubmit}
-      {...formProps}>
-      <label className="usa-sr-only" htmlFor="basic-search-field-small">
+    <Form onSubmit={onSubmit} className={classes} role="search">
+      <Label className="usa-sr-only" htmlFor="basic-search-field-small">
         Search small
-      </label>
-      <input
+      </Label>
+      <TextInput
         className="usa-input"
         id="basic-search-field-small"
         type="search"
         name="search"
       />
-      <button className="usa-button" type="submit">
+      <Button type="submit">
         <span className="usa-sr-only">Search</span>
-      </button>
-    </form>
+      </Button>
+    </Form>
   )
 }
 
