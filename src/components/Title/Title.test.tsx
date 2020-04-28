48f7e918ae58c048efb8fdf00237607c3b67bd1d
eamahanna <emahanna@truss.works>
2020-04-28 07:46:13 -0800

    Fix props

--- src/components/Title/Title.test.tsx
+++ src/components/Title/Title.test.tsx
@@ -1,4 +1,4 @@
-import React, { Children } from 'react'
+import React from 'react'
 import { render } from '@testing-library/react'
 
 import { Title } from './Title'
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,13 +3,18 @@ import classnames from 'classnames'
 
 interface TitleProps {
   children: React.ReactNode
+  className?: string
 }
 
-export const Title = ({ children }: TitleProps): React.ReactElement => {
-  const classes = classnames('usa-logo')
+export const Title = ({
+  className,
+  children,
+  ...props
+}: TitleProps): React.ReactElement => {
+  const classes = classnames('usa-logo', className)
 
   return (
-    <div className={classes}>
+    <div className={classes} {...props}>
       <em className="usa-logo__text">{children}</em>
     </div>
   )
