eacf11f54174e27be78e23d884566f27b9655082
HANA <hana@truss.works>
2020-05-11 12:10:48 -0500

    feat: Big footer (#142)
    * Add extended nav behavior inside FooterNav
    * Add SignupForm
    * Organize stories in subdirectories and add info

--- src/components/Footer/FooterNav/FooterNav.test.tsx
+++ src/components/Footer/FooterNav/FooterNav.test.tsx
@@ -1,4 +1,5 @@
-/*  eslint-disable jsx-a11y/anchor-is-valid */
+/*  eslint-disable jsx-a11y/anchor-is-valid, react/jsx-key */
+
 import React from 'react'
 import { render } from '@testing-library/react'
 
@@ -9,6 +10,26 @@ const links = Array(4).fill(
     Primary Link
   </a>
 )
+
+const extendedLinks = [
+  [
+    'Types of Cats',
+    ...Array(2).fill(
+      <a className="usa-footer__secondary-link" href="#">
+        Cheetah
+      </a>
+    ),
+  ],
+  [
+    'Musical Gifts',
+    ...Array(3).fill(
+      <a className="usa-footer__secondary-link" href="#">
+        Purple Rain
+      </a>
+    ),
+  ],
+]
+
 describe('FooterNav component', () => {
   it('renders without errors', () => {
     const { getByRole } = render(<FooterNav links={links} />)
@@ -19,5 +40,25 @@ describe('FooterNav component', () => {
     const { container, getAllByText } = render(<FooterNav links={links} />)
     expect(container.querySelectorAll('a').length).toBe(4)
     expect(getAllByText('Primary Link').length).toBe(4)
+  })
+
+  it('renders links with "big" prop', () => {
+    const { container, getAllByText } = render(<FooterNav links={links} big />)
+    expect(container.querySelectorAll('a').length).toBe(4)
+    expect(getAllByText('Primary Link').length).toBe(4)
+  })
+
+  it('renders extended links with "big" prop', () => {
+    const { container, getAllByText } = render(
+      <FooterNav links={extendedLinks} big />
+    )
+    expect(container.querySelectorAll('a').length).toBe(5)
+    expect(getAllByText('Purple Rain').length).toBe(3)
+    expect(getAllByText('Cheetah').length).toBe(2)
+  })
+
+  it('does not render extended nav links without "big" prop', () => {
+    const { container } = render(<FooterNav links={extendedLinks} />)
+    expect(container.querySelectorAll('a').length).toBe(0)
   })
 })
--- src/components/Footer/FooterNav/FooterNav.tsx
+++ src/components/Footer/FooterNav/FooterNav.tsx
@@ -1,36 +1,95 @@
 import React from 'react'
 import classnames from 'classnames'
 
+type ExtendedNavLinks = [React.ReactNode[]]
+
 type FooterNavProps = {
   big?: boolean
   medium?: boolean
   slim?: boolean
-  links: React.ReactNode[]
+  /* 
+    Union type. Array of navigation links or multidimensional array of ExtendedNavLinks.
+    ExtendedNavLinks are ordered sub arrays that will be displayed as columns, with the first element used as the section heading.
+    ExtendedNavLinks can only be used with "big" prop size
+  */
+  links: React.ReactNode[] | ExtendedNavLinks
+}
+
+function isExtendedNavLinks(
+  links: React.ReactNode[] | ExtendedNavLinks
+): links is ExtendedNavLinks {
+  return (links as ExtendedNavLinks)[0].constructor === Array
 }
 
 export const FooterNav = (
   props: FooterNavProps & React.HTMLAttributes<HTMLElement>
 ): React.ReactElement => {
-  const { medium, slim, links, ...elementAttributes } = props
+  const { big, medium, slim, links, ...elementAttributes } = props
 
-  const navClasses = classnames(`usa-footer__nav`, elementAttributes.className)
   const listItemClasses = classnames(
     'desktop:grid-col-auto usa-footer__primary-content',
     {
-      'mobile-lg:grid-col-4': medium,
+      'mobile-lg:grid-col-4': big || medium,
       'mobile-lg:grid-col-6': slim,
     }
   )
 
   return (
-    <nav className={navClasses} aria-label="Footer navigation">
-      <ul className="grid-row grid-gap">
-        {links.map((link, i) => (
-          <li key={`navLink-${i}`} className={listItemClasses}>
+    <nav
+      {...elementAttributes}
+      className="usa-footer__nav"
+      aria-label="Footer navigation">
+      {big && isExtendedNavLinks(links) && <ExtendedNav nestedLinks={links} />}
+
+      {!isExtendedNavLinks(links) && (
+        <ul className="grid-row grid-gap">
+          {links.map((link, i) => (
+            <li key={`navLink-${i}`} className={listItemClasses}>
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
+    </nav>
+  )
+}
+
+const Section = ({
+  links,
+}: {
+  links: React.ReactNode[]
+}): React.ReactElement => {
+  const primaryLinkOrHeading = links[0]
+  const secondaryLinks = links.slice(1)
+
+  return (
+    <section className="usa-footer__primary-content usa-footer__primary-content--collapsible">
+      <h4 className="usa-footer__primary-link">{primaryLinkOrHeading}</h4>
+      <ul className="usa-list usa-list--unstyled">
+        {secondaryLinks.map((link, i) => (
+          <li key={`navLink-${i}`} className="usa-footer__secondary-link">
             {link}
           </li>
         ))}
       </ul>
-    </nav>
+    </section>
+  )
+}
+
+const ExtendedNav = ({
+  nestedLinks,
+}: {
+  nestedLinks: ExtendedNavLinks
+}): React.ReactElement => {
+  return (
+    <div className="grid-row grid-gap-4">
+      {nestedLinks.map((links, i) => (
+        <div
+          key={`linkSection-${i}`}
+          className="mobile-lg:grid-col-6 desktop:grid-col-3">
+          <Section links={links} />
+        </div>
+      ))}
+    </div>
   )
 }
