1e44e8e469286afc2279e37a7d4a4ebeb5812d8d
Suzanne Rozier <suz@truss.works>
2020-05-26 17:40:07 -0500

    feat(example-app): add create-react-app example app

---
+++ example/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import '@trussworks/react-uswds/lib/uswds.css'
+import { GovBanner } from '@trussworks/react-uswds'
+
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <GovBanner />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
