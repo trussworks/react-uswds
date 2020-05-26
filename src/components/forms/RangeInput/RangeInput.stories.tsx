8e0179f27145732a6dc5a01661fa5b0e5278b1dd
Duncan <52669884+duncan-truss@users.noreply.github.com>
2020-05-26 12:33:44 -0400

    feat(rangeinput): creates form RangeInput component, test, and stories (#194)
    * feat(rangeinput): creates form RangeInput component, test, and stories
    
    * feat: add RangeInput component to index.ts for entrypoint export
    
    * feat: remove overalpping interface props with HTMLInputAttributes type
    
    * feat: require id and name props, remove label, remove redundant unpack
    
    * feat: remove unused label import in component, show label usage in story
    
    fixes #81

---
+++ src/components/forms/RangeInput/RangeInput.stories.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { RangeInput } from './RangeInput'
+import { Label } from '../Label/Label'
+
+export default {
+  title: 'Forms/RangeInput',
+  parameters: {
+    info: `
+USWDS 2.0 RangeInput component
+
+Source: https://designsystem.digital.gov/components/form-controls/#range
+`,
+  },
+}
+
+const labelChildren = (
+  <>
+    <div>
+      <span>Start:</span> <span>0</span>
+    </div>
+    <div>
+      <span>End:</span> <span>100</span>
+    </div>
+  </>
+)
+
+const labelHint = <>(drag to adjust or use arrow keys)</>
+
+export const defaultRange = (): React.ReactElement => (
+  <>
+    <Label htmlFor="range-slider" hint={labelHint}>
+      {labelChildren}
+    </Label>
+    <RangeInput id="range-slider" name="range" />
+  </>
+)
+
+export const customRange = (): React.ReactElement => (
+  <RangeInput
+    id="custom-range-slider"
+    name="rangeValue"
+    className="dark-theme"
+    min={1}
+    max={11}
+    step={2}
+    defaultValue={3}
+  />
+)
+
+// Only tick marks are shown in Chrome but not with usa-range class currently because the appearance property is set to none
+export const dataListRange = (): React.ReactElement => (
+  <>
+    <RangeInput
+      id="range-slider"
+      name="range"
+      min={0}
+      max={4}
+      defaultValue={2}
+      list="range-list-id"
+    />
+    <datalist id="range-list-id">
+      <option>0</option>
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+    </datalist>
+  </>
+)
---
+++ src/components/forms/RangeInput/RangeInput.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import classnames from 'classnames'
+
+interface RangeInputProps {
+  id: string
+  name: string
+  inputRef?:
+    | string
+    | ((instance: HTMLInputElement | null) => void)
+    | React.RefObject<HTMLInputElement>
+    | null
+    | undefined
+}
+
+export const RangeInput = (
+  props: RangeInputProps & React.InputHTMLAttributes<HTMLInputElement>
+): React.ReactElement => {
+  // Range defaults to min = 0, max = 100, step = 1, and value = (max/2) if not specified.
+  const { className, inputRef, ...inputProps } = props
+
+  const classes = classnames('usa-range', className)
+
+  return (
+    <input
+      data-testid="range"
+      className={classes}
+      ref={inputRef}
+      type="range"
+      {...inputProps}
+    />
+  )
+}
+
+export default RangeInput
