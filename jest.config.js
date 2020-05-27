b07ec1bfba7b4f09eddff404809c07d40dd94777
Suzanne Rozier <suz@truss.works>
2020-05-27 14:09:34 -0500

    chore: fix jest react resolver in lib config

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   testRegex: '(/test/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   moduleNameMapper: {
+    '^react$': '<rootDir>/node_modules/react',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
