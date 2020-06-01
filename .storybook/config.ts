eb3b12f7fae696b21a9f8860889973b92d823c46
Emily Mahanna <56279459+eamahanna@users.noreply.github.com>
2020-06-01 08:57:10 -0800

    ci(happo): add happo.io visual regression testing
    * This PR adds visual regression testing of storybook components using happo.io. For additional information on Happo.io read the docs located at: https://docs.happo.io/docs/getting-started

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,6 +1,7 @@
 import { configure, addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 
+import 'happo-plugin-storybook/register'
 import 'uswds/dist/css/uswds.css'
 import '../src/styles/index.scss'
 import './custom-story.css'
