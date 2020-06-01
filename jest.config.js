cf28086ac5c5b29452b04e0b96ceb62b923345e9
Suzanne Rozier <suz@truss.works>
2020-06-01 12:21:30 -0500

    docs(example-app): add create-react-app example app (#206)
    * feat(example-app): add create-react-app example app
    
    * test(example-app): fix Jest React module config
    
    * chore(example-app): use typescript template
    
    * chore: fix jest react resolver in lib config
    
    * chore(example-app): update readme, remove eject script
    
    * test(example-app): ignore example app in CI tests
    
    * docs(example-app): add docs about SKIP_PREFLIGHT_CHECK

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,11 @@ module.exports = {
   transform: {
     '.(ts|tsx)': 'ts-jest',
   },
-  testPathIgnorePatterns: ['/node_modules/', '/lib/'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/example/'],
   testRegex: '(/test/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   moduleNameMapper: {
+    '^react$': '<rootDir>/node_modules/react',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
