25a007f326488185c611ef8723c9018c97ed7b9a
Suzanne Rozier <suz@truss.works>
2020-06-03 17:10:10 -0500

    WIP Set up basic layout, router

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,28 +1,49 @@
 import React from 'react'
+import { useRoutes, A } from 'hookrouter'
 
 import '@trussworks/react-uswds/lib/uswds.css'
-import { GovBanner } from '@trussworks/react-uswds'
+import {
+  GovBanner,
+  Header,
+  Title,
+  SideNav,
+  GridContainer,
+  Grid,
+} from '@trussworks/react-uswds'
 
-import logo from './logo.svg'
+import HomePage from './pages/Home'
+import TestPage from './pages/Test'
+
 import './App.css'
 
-function App() {
+/* Route component definitions */
+const routes = {
+  '/': () => <HomePage />,
+  '/test': () => <TestPage />,
+}
+
+const App = () => {
+  const routeResult = useRoutes(routes)
+  const navItems = [<A href="/">Home</A>, <A href="/test">Test</A>]
+
   return (
     <div className="App">
       <GovBanner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Header basic>
+        <div className="usa-nav-container">
+          <div className="usa-navbar">
+            <Title>ReactUSWDS Example Application</Title>
+          </div>
+        </div>
+      </Header>
+      <GridContainer>
+        <Grid row gap>
+          <Grid tablet={{ col: 3 }}>
+            <SideNav items={navItems} />
+          </Grid>
+          <Grid col>{routeResult}</Grid>
+        </Grid>
+      </GridContainer>
     </div>
   )
 }
---
+++ example/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = (): React.ReactElement => (
+  <section>
+    <h2>Home page</h2>
+  </section>
+)
+
+export default HomePage
