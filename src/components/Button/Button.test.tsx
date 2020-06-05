3b3f4e7f792c99d662094af11433d662fae1462d
Andrew Hobson <21983+ahobson@users.noreply.github.com>
2020-06-05 13:09:48 -0400

    feat(button): size enum on Button, add deprecationWarning util (#229)
    * feat(button): size enum on Button component
    
    Provide a size enum to eventually replace the small and big boolean props on Button
    
    re #187
    
    * feat(Button): Add deprecation warnings, size preferred
    
    * Log deprecation warnings
    * Prefer size over big/small; test that interaction
    
    * Comment deprecationWarning in production
    
    * Button prop deprecation starts in v1.6.0

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 
+jest.mock('../../deprecation')
+import { deprecationWarning } from '../../deprecation'
 import { Button } from './Button'
 
 describe('Button component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('renders without errors', () => {
     const { queryByTestId } = render(<Button type="button">Click Me</Button>)
     expect(queryByTestId('button')).toBeInTheDocument()
@@ -15,19 +21,17 @@ describe('Button component', () => {
       expect(queryByTestId('button')).toHaveClass('usa-button')
     })
 
-    const optionalClasses = [
+    const optionalBooleanClasses = [
       ['secondary', 'usa-button--secondary'],
       ['base', 'usa-button--base'],
       ['accent', 'usa-button--accent-cool'],
       ['outline', 'usa-button--outline'],
       ['inverse', 'usa-button--inverse'],
-      ['big', 'usa-button--big'],
-      ['small', 'usa-button--small'],
       ['icon', 'usa-button--icon'],
       ['unstyled', 'usa-button--unstyled'],
     ]
 
-    optionalClasses.map((data) => {
+    optionalBooleanClasses.map((data) => {
       it(`${data[1]}`, () => {
         const additionalProps: { [key: string]: boolean } = {}
         additionalProps[data[0]] = true
@@ -39,6 +43,51 @@ describe('Button component', () => {
         )
         expect(queryByTestId('button')).toHaveClass(data[1])
       })
+    })
+
+    it('renders uswds class for size small', () => {
+      const { queryByTestId } = render(
+        <Button type="button" size="small">
+          Click Me
+        </Button>
+      )
+      expect(queryByTestId('button')).toHaveClass('usa-button--small')
+      expect(queryByTestId('button')).not.toHaveClass('usa-button--big')
+      expect(deprecationWarning).toHaveBeenCalledTimes(0)
+    })
+
+    it('renders uswds class for size big', () => {
+      const { queryByTestId } = render(
+        <Button type="button" size="big">
+          Click Me
+        </Button>
+      )
+      expect(queryByTestId('button')).toHaveClass('usa-button--big')
+      expect(queryByTestId('button')).not.toHaveClass('usa-button--small')
+      expect(deprecationWarning).toHaveBeenCalledTimes(0)
+    })
+
+    it('prefers size to deprecated big', () => {
+      Button.bind
+      const { queryByTestId } = render(
+        <Button type="button" size="small" big>
+          Click Me
+        </Button>
+      )
+      expect(queryByTestId('button')).toHaveClass('usa-button--small')
+      expect(queryByTestId('button')).not.toHaveClass('usa-button--big')
+      expect(deprecationWarning).toHaveBeenCalledTimes(1)
+    })
+
+    it('prefers size to deprecated small', () => {
+      const { queryByTestId } = render(
+        <Button type="button" size="big" small>
+          Click Me
+        </Button>
+      )
+      expect(queryByTestId('button')).toHaveClass('usa-button--big')
+      expect(queryByTestId('button')).not.toHaveClass('usa-button--small')
+      expect(deprecationWarning).toHaveBeenCalledTimes(1)
     })
   })
 
