832decc83ec38afefed86a25409a0a9ebce83423
Suzanne Rozier <suz@truss.works>
2020-06-15 13:00:03 -0500

    docs(example-app): Example app UI #202 (#237)
    * WIP Set up basic layout, router
    
    * Switch to react-router, add placeholder content
    
    * Add docs and commands for running the example app

---
+++ example/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = (): React.ReactElement => (
+  <main>
+    <h1>ReactUSWDS Example App</h1>
+
+    <p className="usa-intro">
+      This is an example application that can be used to demonstrate and test
+      ReactUSWDS functionality.
+    </p>
+  </main>
+)
+
+export default HomePage
