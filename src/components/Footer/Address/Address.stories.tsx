be5d62ca39b05ea5a18f238f2f3fdf0e43e59df1
Andrew Hobson <ahobson@truss.works>
2020-06-17 15:58:25 -0400

    feat(Address): convert to enum size prop, deprecate boolean
    * closes #265

--- src/components/Footer/Address/Address.stories.tsx
+++ src/components/Footer/Address/Address.stories.tsx
@@ -6,7 +6,7 @@ export default {
   parameters: {
     info: `
       Display address items (most likely links or simple text) in a row, wrapped in address tag.  Used in USWDS 2.0 Footer component.
-      
+
       Source: https://designsystem.digital.gov/components/form-controls/#footer
       `,
   },
