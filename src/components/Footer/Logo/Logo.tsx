ea88c500e31ca80950747f62b310b6ed7d7a8df1
Andrew Hobson <21983+ahobson@users.noreply.github.com>
2020-06-24 10:53:28 -0400

    feat(Logo): use size prop, deprecate boolean types (#268)
    * fixes #264

--- src/components/Footer/Logo/Logo.tsx
+++ src/components/Footer/Logo/Logo.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import classnames from 'classnames'
+import { deprecationWarning } from '../../../deprecation'
 
 type LogoProps = {
+  size?: 'big' | 'medium' | 'slim'
+  /**
+   * @deprecated since 1.6.0, use size
+   */
   big?: boolean
+  /**
+   * @deprecated since 1.6.0, use size
+   */
   medium?: boolean
+  /**
+   * @deprecated since 1.6.0, use size
+   */
   slim?: boolean
   heading?: React.ReactNode
   image: React.ReactNode
 }
 
 export const Logo = ({
+  size,
   big,
   medium,
   slim,
@@ -17,18 +29,31 @@ export const Logo = ({
   image,
   ...elementAttributes
 }: LogoProps & React.HtmlHTMLAttributes<HTMLElement>): React.ReactElement => {
+  if (big) {
+    deprecationWarning('FooterNav property big is deprecated.  Use size')
+  }
+  if (medium) {
+    deprecationWarning('FooterNav property medium is deprecated.  Use size')
+  }
+  if (slim) {
+    deprecationWarning('FooterNav property slim is deprecated.  Use size')
+  }
+  const isBig = size ? size === 'big' : big
+  const isMedium = size ? size === 'medium' : medium
+  const isSlim = size ? size === 'slim' : slim
+
   const containerClasses = classnames(
     'usa-footer__logo grid-row',
     {
-      'mobile-lg:grid-col-6 mobile-lg:grid-gap-2': big || medium,
-      'grid-gap-2': slim,
+      'mobile-lg:grid-col-6 mobile-lg:grid-gap-2': isBig || isMedium,
+      'grid-gap-2': isSlim,
     },
     elementAttributes.className
   )
 
   const columnClasses = classnames({
-    'mobile-lg:grid-col-auto': big || medium,
-    'grid-col-auto': slim,
+    'mobile-lg:grid-col-auto': isBig || isMedium,
+    'grid-col-auto': isSlim,
   })
 
   return (
