f2d653e115d36ceea9497c12e1abfc3384ba04a6
Andrew Hobson <21983+ahobson@users.noreply.github.com>
2020-06-26 16:52:53 -0400

    feat(TextInput): use enum props, deprecate booleans (#284)
    * feat(TextInput): use enum props, deprecate booleans
    
    * closes #283
    
    * feat(TextInput): use inputSize instead of sizeVariant

--- src/components/forms/TextInput/TextInput.tsx
+++ src/components/forms/TextInput/TextInput.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import classnames from 'classnames'
+import { deprecationWarning } from '../../../deprecation'
 
 interface TextInputProps {
   id: string
   name: string
   type: 'text' | 'email' | 'number' | 'password' | 'search' | 'tel' | 'url'
   className?: string
+  validationStatus?: 'error' | 'success'
+  /**
+   * @deprecated since 1.6.0, use validationStatus
+   */
   error?: boolean
+  /**
+   * @deprecated since 1.6.0, use validationStatus
+   */
   success?: boolean
+  inputSize?: 'small' | 'medium'
+  /**
+   * @deprecated since 1.6.0, use inputSize
+   */
   small?: boolean
+  /**
+   * @deprecated since 1.6.0, use inputSize
+   */
   medium?: boolean
   inputRef?:
     | string
@@ -26,21 +41,45 @@ export const TextInput = (
     name,
     type,
     className,
+    validationStatus,
     error,
     success,
+    inputSize,
     small,
     medium,
     inputRef,
     ...inputProps
   } = props
+  if (error) {
+    deprecationWarning(
+      'TextInput property error is deprecated.  Use validationStatus'
+    )
+  }
+  if (success) {
+    deprecationWarning(
+      'TextInput property success is deprecated.  Use validationStatus'
+    )
+  }
+  if (small) {
+    deprecationWarning('TextInput property small is deprecated.  Use inputSize')
+  }
+  if (medium) {
+    deprecationWarning(
+      'TextInput property medium is deprecated.  Use inputSize'
+    )
+  }
 
+  const isError = validationStatus ? validationStatus === 'error' : error
+  const isSuccess = validationStatus ? validationStatus === 'success' : success
+  const isSmall = inputSize ? inputSize === 'small' : small
+  const isMedium = inputSize ? inputSize === 'medium' : medium
   const classes = classnames(
     'usa-input',
     {
-      'usa-input--error': error,
-      'usa-input--success': success,
-      'usa-input--small': small,
-      'usa-input--medium': medium,
+      'usa-input--error': isError,
+      'usa-input--success': isSuccess,
+      'usa-input--small': isSmall,
+      'usa-input--medium': isMedium,
     },
     className
   )
