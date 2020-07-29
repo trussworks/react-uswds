6fe1030dc67225f41932bbe570b448fcfa63ba73
Hana Worku <hana@truss.works>
2020-07-29 12:49:38 -0500

    Add component and stories

---
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import classnames from 'classnames'
+
+// Types
+type ButtonGroupProps = {
+  children: React.ReactNode
+  className?: string
+  type?: 'default' | 'segmented'
+} & JSX.IntrinsicElements['ul']
+
+export function ButtonGroup<FCProps = ButtonGroupProps>(
+  props: ButtonGroupProps
+): React.ReactElement {
+  const { className, children, type = 'default', ...restProps } = props
+
+  const classes = classnames(
+    ' usa-button-group',
+    {
+      'usa-button-group--segmented': type == 'segmented',
+    },
+    className
+  )
+
+  return (
+    <ul data-testid="buttonGroup" className={classes} {...restProps}>
+      {React.Children.map(children, (child: React.ReactNode) => {
+        return <li className="usa-button-group__item">{child}</li>
+      })}
+    </ul>
+  )
+}
+
+export default ButtonGroup
