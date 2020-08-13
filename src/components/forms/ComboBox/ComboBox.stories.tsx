60eb61be2229abab15d0f7d8ca01b77a3b1be6ab
Jim Benton <jim@truss.works>
2020-08-13 10:20:36 -0500

    Arrow keys and escape are working.

--- src/components/forms/ComboBox/ComboBox.stories.tsx
+++ src/components/forms/ComboBox/ComboBox.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { ComboBox } from './ComboBox'
-import { Label } from '../Label/Label'
 import { Form } from '../Form/Form'
 
 export default {
@@ -20,38 +19,100 @@ const noop = (): void => {
   return
 }
 
-export const defaultComboBox = (): React.ReactElement => (
-  <Form onSubmit={noop}>
-    <ComboBox id="input-ComboBox" name="input-ComboBox"></ComboBox>
-  </Form>
-)
+const fruits = {
+  apple: 'Apple',
+  apricot: 'Apricot',
+  avocado: 'Avocado',
+  banana: 'Banana',
+  blackberry: 'Blackberry',
+  'blood orange': 'Blood orange',
+  blueberry: 'Blueberry',
+  boysenberry: 'Boysenberry',
+  breadfruit: 'Breadfruit',
+  'buddhas hand citron': "Buddha's hand citron",
+  cantaloupe: 'Cantaloupe',
+  clementine: 'Clementine',
+  'crab apple': 'Crab apple',
+  currant: 'Currant',
+  cherry: 'Cherry',
+  'custard apple': 'Custard apple',
+  coconut: 'Coconut',
+  cranberry: 'Cranberry',
+  date: 'Date',
+  dragonfruit: 'Dragonfruit',
+  durian: 'Durian',
+  elderberry: 'Elderberry',
+  fig: 'Fig',
+  gooseberry: 'Gooseberry',
+  grape: 'Grape',
+  grapefruit: 'Grapefruit',
+  guava: 'Guava',
+  'honeydew melon': 'Honeydew melon',
+  jackfruit: 'Jackfruit',
+  kiwifruit: 'Kiwifruit',
+  kumquat: 'Kumquat',
+  lemon: 'Lemon',
+  lime: 'Lime',
+  lychee: 'Lychee',
+  mandarine: 'Mandarine',
+  mango: 'Mango',
+  mangosteen: 'Mangosteen',
+  marionberry: 'Marionberry',
+  nectarine: 'Nectarine',
+  orange: 'Orange',
+  papaya: 'Papaya',
+  passionfruit: 'Passionfruit',
+  peach: 'Peach',
+  pear: 'Pear',
+  persimmon: 'Persimmon',
+  plantain: 'Plantain',
+  plum: 'Plum',
+  pineapple: 'Pineapple',
+  pluot: 'Pluot',
+  pomegranate: 'Pomegranate',
+  pomelo: 'Pomelo',
+  quince: 'Quince',
+  raspberry: 'Raspberry',
+  rambutan: 'Rambutan',
+  soursop: 'Soursop',
+  starfruit: 'Starfruit',
+  strawberry: 'Strawberry',
+  tamarind: 'Tamarind',
+  tangelo: 'Tangelo',
+  tangerine: 'Tangerine',
+  'ugli fruit': 'Ugli fruit',
+  watermelon: 'Watermelon',
+  'white currant': 'White currant',
+  yuzu: 'Yuzu',
+}
 
-export const withDefaultValue = (): React.ReactElement => (
-  <ComboBox id="input-ComboBox" name="input-ComboBox" defaultValue="value2">
-    <option>- Select - </option>
-    <option value="value1">Option A</option>
-    <option value="value2">Option B</option>
-    <option value="value3">Option C</option>
-  </ComboBox>
-)
+export const defaultComboBoxWithPropOptions = (): React.ReactElement => {
+  const opts = Object.entries(fruits).map(([key, value]) => ({
+    value: value,
+    label: key,
+  }))
 
-export const withLabel = (): React.ReactElement => (
-  <>
-    <Label htmlFor="options">ComboBox label</Label>
-    <ComboBox id="input-ComboBox" name="input-ComboBox">
-      <option>- Select - </option>
-      <option value="value1">Option A</option>
-      <option value="value2">Option B</option>
-      <option value="value3">Option C</option>
-    </ComboBox>
-  </>
-)
+  return (
+    <Form onSubmit={noop}>
+      <ComboBox id="input-ComboBox" name="input-ComboBox" options={opts} />
+    </Form>
+  )
+}
 
-export const disabled = (): React.ReactElement => (
-  <ComboBox id="input-ComboBox" name="input-ComboBox" disabled>
-    <option>- Select - </option>
-    <option value="value1">Option A</option>
-    <option value="value2">Option B</option>
-    <option value="value3">Option C</option>
-  </ComboBox>
-)
+// export const withDefaultValue = (): React.ReactElement => (
+//   <ComboBox id="input-ComboBox" name="input-ComboBox" defaultValue="value2">
+//   </ComboBox>
+// )
+
+// export const withLabel = (): React.ReactElement => (
+//   <>
+//     <Label htmlFor="options">ComboBox label</Label>
+//     <ComboBox id="input-ComboBox" name="input-ComboBox">
+//     </ComboBox>
+//   </>
+// )
+
+// export const disabled = (): React.ReactElement => (
+//   <ComboBox id="input-ComboBox" name="input-ComboBox" disabled>
+//   </ComboBox>
+// )
--- src/components/forms/ComboBox/ComboBox.tsx
+++ src/components/forms/ComboBox/ComboBox.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { KeyboardEvent, useState, useEffect, useRef } from 'react'
 import classnames from 'classnames'
+
+interface ComboBoxOption {
+  value: string
+  label?: string
+}
+
+enum Direction {
+  Up = -1,
+  Down = 1,
+}
 
 interface ComboBoxProps {
   id: string
-  name: string // how to handle this?
+  name: string
   className?: string
+  options: ComboBoxOption[]
 }
 
 export const ComboBox = (
-  props: ComboBoxProps // & JSX.IntrinsicElements['select']
+  props: ComboBoxProps & JSX.IntrinsicElements['select']
 ): React.ReactElement => {
-  const { id, className, ...inputProps } = props
+  const { id, name, className, options, ...selectProps } = props
+
+  const [inputValue, setInputValue] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedValue, setSelectedValue] = useState<string>()
 
   const classes = classnames('usa-combo-box', className)
 
@@ -19,26 +34,98 @@ export const ComboBox = (
   const assistiveHintID = ''
   const selectID = ''
 
+  const itemRef = useRef<HTMLLIElement>(null)
+  useEffect(() => {
+    if (selectedValue && itemRef.current) {
+      itemRef.current.focus()
+    }
+  })
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  useEffect(() => {
+    if (!selectedValue && inputRef.current) {
+      inputRef.current.focus()
+    }
+  })
+
+  const moveSelection = (change: Direction): void => {
+    const currentIndex = options.findIndex(
+      (option) => option.value === selectedValue
+    )
+    if (currentIndex === -1) {
+      const newOption = options[0]
+      setSelectedValue(newOption.value)
+    } else {
+      const newIndex = currentIndex + change
+      if (newIndex < 0) {
+        setIsOpen(false)
+        setSelectedValue(undefined)
+      } else {
+        // eslint-disable-next-line security/detect-object-injection
+        const newOption = options[newIndex]
+        setSelectedValue(newOption.value)
+      }
+    }
+  }
+
+  const handleInputKeyDown = (event: KeyboardEvent): void => {
+    if (event.key == 'Escape') {
+      setIsOpen(false)
+    } else if (event.key == 'ArrowDown' || event.key == 'Down') {
+      event.preventDefault()
+      moveSelection(1)
+      setIsOpen(true)
+    }
+  }
+
+  const handleListItemKeyDown = (event: KeyboardEvent): void => {
+    if (event.key == 'Escape') {
+      setIsOpen(false)
+      setSelectedValue(undefined)
+      setInputValue('')
+    } else if (event.key == 'ArrowDown' || event.key == 'Down') {
+      event.preventDefault()
+      moveSelection(1)
+    } else if (event.key == 'ArrowUp' || event.key == 'Up') {
+      event.preventDefault()
+      moveSelection(-1)
+    }
+  }
+
   return (
-    <div
-      data-testid="combo-box"
-      className={classes}
-      id={id}
-      // name={name}
-      {...inputProps}>
+    <div data-testid="combo-box" className={classes} id={id}>
+      <select
+        className="usa-select usa-sr-only usa-combo-box__select"
+        name={name}
+        aria-hidden
+        tabIndex={-1}
+        value={selectedValue}
+        {...selectProps}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label || option.value}
+          </option>
+        ))}
+      </select>
       <input
         aria-owns={listID}
         aria-autocomplete="list"
         aria-describedby={assistiveHintID}
-        aria-expanded="false"
+        aria-expanded={isOpen}
         aria-controls=""
-        // autocapitalize="off"
-        // autocomplete="off"
         id={selectID}
         className="usa-combo-box__input"
         type="text"
         role="combobox"
-        // ${additionalAttributes.join(" ")
+        onClick={(): void => setIsOpen(true)}
+        ref={inputRef}
+        // onBlur={(): void => setIsOpen(false)}
+        onKeyDown={handleInputKeyDown}
+        value={inputValue}
+        onChange={(e): void => setInputValue(e.target.value)}
+        // these do not type check:
+        // autocapitalize="off"
+        // autocomplete="off"
       />
       <span className="usa-combo-box__clear-input__wrapper" tabIndex={-1}>
         <button
@@ -63,7 +150,32 @@ export const ComboBox = (
         id={listID}
         className="usa-combo-box__list"
         role="listbox"
-        hidden></ul>
+        hidden={!isOpen}>
+        {options.map((option, index) => {
+          const selected = option.value === selectedValue
+          const itemClasses = classnames('usa-combo-box__list-option', {
+            'usa-combo-box__list-option--focused': selected,
+          })
+
+          return (
+            <li
+              ref={selected ? itemRef : null}
+              value={option.value}
+              key={option.value}
+              className={itemClasses}
+              tabIndex={selected ? 0 : -1}
+              role="option"
+              aria-selected={selected}
+              aria-setsize={64}
+              aria-posinset={index + 1}
+              id={listID + `--option-${index}`}
+              onKeyDown={handleListItemKeyDown}>
+              {option.label || option.value}
+            </li>
+          )
+        })}
+      </ul>
+
       <div className="usa-combo-box__status usa-sr-only" role="status"></div>
       <span id={assistiveHintID} className="usa-sr-only">
         When autocomplete results are available use up and down arrows to review
