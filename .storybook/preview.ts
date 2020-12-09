c8dc6528f8a1590acd869d41bcbf2ea2c1793cf7
Hana Worku <hana@truss.works>
2020-12-09 12:22:15 -0600

    Fix typography bug: use theme-global-link-styles, import all uswds scss

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,5 @@
 import 'happo-plugin-storybook/register'
 import './index.scss'
-import 'uswds/dist/css/uswds.css'
 
 import { addParameters } from '@storybook/client-api'
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
