96b739a27ec1e15172dc0d80f75b8e1b8416993a
Brandon Lenz <brandon@truss.works>
2021-01-11 20:34:18 -0500

    Add GovBanner stories

--- src/components/GovBanner/GovBanner.stories.tsx
+++ src/components/GovBanner/GovBanner.stories.tsx
@@ -3,6 +3,38 @@ import { GovBanner } from './GovBanner'
 
 export default { title: 'GovBanner', component: GovBanner }
 
-export const govBanner = (): React.ReactElement => (
+export const govBannerDefault = (): React.ReactElement => (
   <GovBanner aria-label="Official government website" />
+)
+
+export const govBannerEnglishDotGov = (): React.ReactElement => (
+  <GovBanner
+    language="en"
+    tld=".gov"
+    aria-label="Official government website"
+  />
+)
+
+export const govBannerEnglishDotMil = (): React.ReactElement => (
+  <GovBanner
+    language="en"
+    tld=".mil"
+    aria-label="Official government website"
+  />
+)
+
+export const govBannerSpanishDotGov = (): React.ReactElement => (
+  <GovBanner
+    language="es"
+    tld=".gov"
+    aria-label="Official government website"
+  />
+)
+
+export const govBannerSpanishDotMil = (): React.ReactElement => (
+  <GovBanner
+    language="es"
+    tld=".mil"
+    aria-label="Official government website"
+  />
 )
