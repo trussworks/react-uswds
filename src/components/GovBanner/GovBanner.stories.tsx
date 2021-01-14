3d61f78eb0414fe77eba2b7c424560c2f4563721
Brandon Lenz <brandonalenz@gmail.com>
2021-01-14 12:19:36 -0500

    feat: Update GovBanner to match USWDS 2.8.0 release (#782)
    * Update GovBanner to correspond with uswds 2.8.0
    
    * Add GovBanner storybook stories
    
    * Add basic test verifying component render w/ props
    
    * Fix typo "Offical" -> "Official"
    
    * Add Jest Snapshot tests

--- src/components/GovBanner/GovBanner.stories.tsx
+++ src/components/GovBanner/GovBanner.stories.tsx
@@ -3,6 +3,38 @@ import { GovBanner } from './GovBanner'
 
 export default { title: 'GovBanner', component: GovBanner }
 
-export const govBanner = (): React.ReactElement => (
+export const govBannerDefault = (): React.ReactElement => (
   <GovBanner aria-label="Official government website" />
+)
+
+export const govBannerEnglishDotGov = (): React.ReactElement => (
+  <GovBanner
+    language="english"
+    tld=".gov"
+    aria-label="Official government website"
+  />
+)
+
+export const govBannerEnglishDotMil = (): React.ReactElement => (
+  <GovBanner
+    language="english"
+    tld=".mil"
+    aria-label="Official government website"
+  />
+)
+
+export const govBannerSpanishDotGov = (): React.ReactElement => (
+  <GovBanner
+    language="spanish"
+    tld=".gov"
+    aria-label="Official government website"
+  />
+)
+
+export const govBannerSpanishDotMil = (): React.ReactElement => (
+  <GovBanner
+    language="spanish"
+    tld=".mil"
+    aria-label="Official government website"
+  />
 )
