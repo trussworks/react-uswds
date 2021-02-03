4e87515cf64dedddc6dd2fde3c5a35604e970d5e
Suzanne Rozier <suz@truss.works>
2021-02-03 11:06:49 -0600

    Update yarn publish scripts, move dependencies to devDependencies

--- src/components/forms/ComboBox/ComboBox.test.tsx
+++ src/components/forms/ComboBox/ComboBox.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent, prettyDOM } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 import { ComboBox } from './ComboBox'
