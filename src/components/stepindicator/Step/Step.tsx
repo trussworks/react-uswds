caef0355decf611ef9127b2acb4bdae01cee2017
Brandon Lenz <brandon@truss.works>
2021-02-16 19:57:13 -0500

    WIP Commit

---
+++ src/components/stepindicator/Step/Step.tsx
@@ -0,0 +1,51 @@
+import classnames from 'classnames'
+import React from 'react'
+
+enum Status {
+  COMPLETE = 'complete',
+  CURRENT = 'current',
+  INCOMPLETE = 'incomplete',
+}
+
+export interface StepProps {
+  label: string
+  status: Status
+  className: string
+}
+
+export const Step = (
+  props: StepProps & JSX.IntrinsicElements['li']
+): React.ReactElement => {
+  const { label, status = Status.INCOMPLETE, className, ...liProps } = props
+
+  const classes = classnames(
+    'usa-step-indicator__segment',
+    {
+      'usa-step-indicator__segment--complete': status === Status.COMPLETE,
+      'usa-step-indicator__segment--current': status === Status.CURRENT,
+    },
+    className
+  )
+
+  return (
+    <li
+      className={classes}
+      aria-current={status === Status.CURRENT ? 'true' : undefined}
+      {...liProps}>
+      <span className="usa-step-indicator__segment-label">
+        {label}
+        &nbsp;
+        {
+          status === Status.COMPLETE && (
+            <span className="usa-sr-only">completed</span>
+          ) /*TODO: localize*/
+        }
+        {
+          status === Status.INCOMPLETE && (
+            <span className="usa-sr-only">not completed</span>
+          ) /*TODO: localize*/
+        }
+      </span>
+    </li>
+  )
+}
---
+++ src/components/stepindicator/StepIndicator/StepIndicator.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import classnames from 'classnames'
+import { StepProps } from '../Step/Step'
+
+//TODO: Localizations
+
+interface StepIndicatorProps {
+  showLabels?: boolean
+  counters?: true | false | 'small'
+  centered?: boolean
+  children: React.ReactElement<StepProps>[]
+  className?: string
+  divProps?: JSX.IntrinsicElements['div']
+  listProps?: JSX.IntrinsicElements['ol']
+}
+export const StepIndicator = (
+  props: StepIndicatorProps
+): React.ReactElement => {
+  const {
+    showLabels = true,
+    counters = false,
+    centered = false,
+    children,
+    className,
+    divProps,
+    listProps,
+  } = props
+
+  const classes = classnames(
+    'usa-step-indicator',
+    {
+      'usa-step-indicator--no-labels': !showLabels,
+      'usa-step-indicator--counters': counters === true,
+      'usa-step-indicator--counters-sm': counters === 'small',
+      'usa-step-indicator--center': centered,
+    },
+    className
+  )
+
+  const currentStepIndex = children.findIndex(
+    (step) => step.props.status === 'current' || 0
+  )
+  const currentStep = currentStepIndex + 1
+  const currentStepLabel = children[currentStepIndex].props.label
+  const totalSteps = children.length
+
+  return (
+    <div className={classes} {...divProps}>
+      <ol className="usa-step-indicator__segments" {...listProps}>
+        {children}
+      </ol>
+      <div className="usa-step-indicator__header">
+        <h2 className="usa-step-indicator__heading">
+          <span className="usa-step-indicator__heading-counter">
+            <span className="usa-sr-only">Step{/*TODO: localize*/}</span>
+            <span className="usa-step-indicator__current-step">
+              {currentStep}
+            </span>
+            <span className="usa-step-indicator__total-steps">{
+              `of ${totalSteps}` /*TODO: localize*/
+            }</span>
+          </span>
+          <span className="usa-step-indicator__heading-text">
+            {currentStepLabel}
+          </span>
+        </h2>
+      </div>
+    </div>
+  )
+}
