ec2525f7c6b1c4ef034744e0668727234eb3ff52
Noelledusahel <barberossa@gmail.com>
2021-03-02 14:07:28 -0500

    corect var name CustomLinkProps

--- src/components/Tooltip/Tooltop.stories.tsx
+++ src/components/Tooltip/Tooltop.stories.tsx
@@ -52,14 +52,14 @@ export const tooltipLeft = (): React.ReactElement => (
 )
 
 export const CustomComponent = (): React.ReactElement => {
-  type MockTooltipProps = React.PropsWithChildren<{
+  type CustomLinkProps = React.PropsWithChildren<{
     to: string
     className?: string
   }> &
     JSX.IntrinsicElements['a'] &
     React.RefAttributes<HTMLAnchorElement>
-  const CustomLink: React.ForwardRefExoticComponent<MockTooltipProps> = React.forwardRef(
-    ({ to, className, children, ...tooltipProps }: MockTooltipProps, ref) => (
+  const CustomLink: React.ForwardRefExoticComponent<CustomLinkProps> = React.forwardRef(
+    ({ to, className, children, ...tooltipProps }: CustomLinkProps, ref) => (
       <a ref={ref} href={to} className={className} {...tooltipProps}>
         {children}
       </a>
@@ -71,7 +71,7 @@ export const CustomComponent = (): React.ReactElement => {
   return (
     <div className="margin-4">
       <p>
-        <Tooltip<MockTooltipProps>
+        <Tooltip<CustomLinkProps>
           label="Follow Link"
           asCustom={CustomLink}
           to="http://www.truss.works">
--- src/components/Tooltip/utils.ts
+++ src/components/Tooltip/utils.ts
@@ -1,4 +1,3 @@
-
 export const isElementInViewport = (
   el: HTMLElement,
   win = window,
