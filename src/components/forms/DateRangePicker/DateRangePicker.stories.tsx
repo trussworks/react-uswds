997d6458f97adf4493f75a8efb58c8751521e6b6
Brandon Lenz <brandon@truss.works>
2021-03-03 11:45:38 -0500

    Add default value story

--- src/components/forms/DateRangePicker/DateRangePicker.stories.tsx
+++ src/components/forms/DateRangePicker/DateRangePicker.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { DateRangePicker } from './DateRangePicker'
 import { Form } from '../Form/Form'
+import { addDays, formatDate } from '../DatePicker/utils'
 
 export default {
   title: 'Components/Form controls/Date range picker',
@@ -46,6 +47,29 @@ export const completeDateRangePicker = (argTypes): React.ReactElement => (
       endDatePickerProps={{
         id: "event-date-end",
         name: "event-date-end",
+        disabled: argTypes.endDatePickerDisabled
+      }}
+    />
+  </Form> 
+)
+
+export const withDefaultValues = (argTypes): React.ReactElement => (
+  <Form onSubmit={argTypes.onSubmit}>
+    <DateRangePicker 
+      startDateLabel="Event start date"
+      startDateHint="mm/dd/yyyy"
+      startDatePickerProps={{
+        id: "event-date-start",
+        name: "event-date-start",
+        defaultValue: formatDate(new Date()),
+        disabled: argTypes.startDatePickerDisabled
+      }}
+      endDateLabel="Event end date"
+      endDateHint="mm/dd/yyyy"
+      endDatePickerProps={{
+        id: "event-date-end",
+        name: "event-date-end",
+        defaultValue: formatDate(addDays(new Date(), 5)),
         disabled: argTypes.endDatePickerDisabled
       }}
     />
