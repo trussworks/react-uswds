a50949c00eb53aaecec3746ef4bba5696381491e
Noelledusahel <barberossa@gmail.com>
2021-03-18 11:58:36 -0400

    reformat for storybook addon

--- src/components/Tooltip/Tooltop.stories.tsx
+++ src/components/Tooltip/Tooltop.stories.tsx
@@ -5,11 +5,15 @@ export default {
   title: 'Components/Tooltip',
   component: Tooltip,
   parameters: {
-    info: `
-    USWDS 2.0 Tooltip component
+    docs: {
+      description: {
+        component: `
+### USWDS 2.0 Tooltip component
 
-    Source: https://designsystem.digital.gov/components/tooltip/
-    `,
+Source: https://designsystem.digital.gov/components/tooltip/
+`,
+      },
+    },
   },
 }
 
