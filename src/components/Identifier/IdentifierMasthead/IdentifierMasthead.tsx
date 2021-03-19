1bf8d713febd1aaba4edfc709c23f789db7a4d68
Arianna Kellogg <ari@truss.works>
2021-03-18 19:33:59 -0700

    Adding bare bones structure based on USWDS code to IdentifierMasthead

--- src/components/Identifier/IdentifierMasthead/IdentifierMasthead.tsx
+++ src/components/Identifier/IdentifierMasthead/IdentifierMasthead.tsx
@@ -19,7 +19,18 @@ export const IdentifierMasthead = (
       data-testid="identifierMasthead"
       className={classes}
       {...sectionProps}>
-      <div>identifier masthead</div>
+      <div className="usa-identifier__container">
+        <div className="usa-identifier__logos">
+          <a href="www.google.com" className="usa-identifier__logo">
+            <img
+              className="usa-identifier__logo-img"
+              // src="/assets/img/circle-gray-20.svg"
+
+              alt="&lt;Parent agency&gt; logo"
+            />
+          </a>
+        </div>
+      </div>
     </section>
   )
 }
@@ -33,4 +44,30 @@ export default IdentifierMasthead
     - agency descrip
     - identify domain
     - identity disclaimer (and link)
+
+
+<div class="usa-identifier">
+  <section
+    class="usa-identifier__section usa-identifier__section--masthead"
+    aria-label="Agency identifier">
+    <div class="usa-identifier__container">
+    <div class="usa-identifier__logos">
+        <a href="javascript:void(0);" class="usa-identifier__logo">
+          <img
+            class="usa-identifier__logo-img"
+            
+            src="/assets/img/circle-gray-20.svg"
+            
+            
+            alt="&lt;Parent agency&gt; logo"
+            
+            role="img">
+        </a>
+        </div>
+        <div class="usa-identifier__identity" aria-label="Agency description">
+        <p class="usa-identifier__identity-domain">domain.gov</p>
+        <p class="usa-identifier__identity-disclaimer">An official website of the <a href="javascript:void(0);">&lt;Parent agency&gt;</a></p>
+      </div>
+    </div>
+  </section>
 */
