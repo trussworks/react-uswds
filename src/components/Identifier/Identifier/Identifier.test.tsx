4ccf711631f68aa3cec8e14321c8bc3f94afba14
Arianna Kellogg <ari@truss.works>
2021-03-29 14:34:14 -0700

    Remove superfluous import

--- src/components/Identifier/Identifier/Identifier.test.tsx
+++ src/components/Identifier/Identifier/Identifier.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { queryByText, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import { Identifier } from './Identifier'
 import { IdentifierMasthead } from '../IdentifierMasthead/IdentifierMasthead'
