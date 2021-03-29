0cd4c90041f99e6397abafafa64b238d06b4acea
Arianna Kellogg <ari@truss.works>
2021-03-29 15:01:51 -0700

    Update IdentifierMasthead tests with new structure and test data, check for rendering without errors, rendering in Spanish, rendering without a logo, rendering more than two logos

--- src/components/Identifier/IdentifierMasthead/IdentifierMasthead.test.tsx
+++ src/components/Identifier/IdentifierMasthead/IdentifierMasthead.test.tsx
@@ -6,66 +6,102 @@ import dotGovIcon from 'uswds/src/img/icon-dot-gov.svg'
 import { IdentifierMasthead } from './IdentifierMasthead'
 import { IdentifierLogo } from '../IdentifierLogo/IdentifierLogo'
 
+const testIdentifierLogo = [
+  <img
+    key="three"
+    src={dotGovIcon}
+    className="usa-identifier__logo-img"
+    alt="Test Agency Name logo"
+  />,
+]
+
+const testIdentifierLogoSpanish = [
+  <img
+    key="four"
+    src={dotGovIcon}
+    className="usa-identifier__logo-img"
+    alt="Logo de Test Agency Name"
+  />,
+]
+
 describe('IdentifierMasthead component', () => {
   it('renders without errors', () => {
-    const { queryByTestId } = render(
-      <IdentifierMasthead>
-        <IdentifierLogo
-          agencyUrl="#"
-          agencyName="Test Agency Name"
-          src={dotGovIcon}
-        />
+    const { getAllByRole, queryByTestId } = render(
+      <IdentifierMasthead
+        className="usa-identifier__custom-class-name"
+        aria-label="Agency identifier">
+        <div className="usa-identifier__container">
+          <div className="usa-identifier__logos">
+            <IdentifierLogo agencyUrl="#" className="custom-class-name">
+              {testIdentifierLogo}
+            </IdentifierLogo>
+          </div>
+          <div
+            data-testid="identifierMasthead-agency-description"
+            className="usa-identifier__identity"
+            aria-label="Agency description">
+            <p className="usa-identifier__identity-domain">
+              domain.edu.mil.gov
+            </p>
+            <p className="usa-identifier__identity-disclaimer">
+              {`An official website of the `}
+              <a href="testlink">Test Agency Name</a>
+            </p>
+          </div>
+        </div>
       </IdentifierMasthead>
     )
+
+    expect(queryByTestId('identifierMasthead-logo')).toBeInTheDocument
+    expect(getAllByRole('link')).toHaveLength(2)
     expect(queryByTestId('identifierMasthead')).toBeInTheDocument()
-  })
-
-  it('renders section attributes passed in by props', () => {
-    const { queryByTestId } = render(
-      <IdentifierMasthead aria-label="custom aria-label value">
-        <IdentifierLogo
-          agencyUrl="#"
-          agencyName="Test Agency Name"
-          src={dotGovIcon}
-        />
-      </IdentifierMasthead>
-    )
-    expect(queryByTestId('identifierMasthead')).toHaveAttribute(
-      'aria-label',
-      'custom aria-label value'
-    )
-  })
-
-  it('renders with a custom className passed in', () => {
-    const { queryByTestId } = render(
-      <IdentifierMasthead className="usa-identifier__custom-class-name">
-        <IdentifierLogo
-          agencyUrl="#"
-          agencyName="Test Agency Name"
-          src={dotGovIcon}
-        />
-      </IdentifierMasthead>
-    )
     expect(queryByTestId('identifierMasthead')).toHaveClass(
       'usa-identifier__section usa-identifier__section--masthead usa-identifier__custom-class-name'
     )
   })
 
-  it('renders with a logo passed in', () => {
-    const { queryByTestId } = render(
-      <IdentifierMasthead>
-        <IdentifierLogo
-          agencyUrl="#"
-          agencyName="Test Agency Name"
-          src={dotGovIcon}
-        />
-      </IdentifierMasthead>
+  it('renders in Spanish', () => {
+    const { queryByAltText } = render(
+      <IdentifierMasthead>{testIdentifierLogoSpanish}</IdentifierMasthead>
     )
-    expect(queryByTestId('identifierMasthead-logo')).toBeInTheDocument
+    expect(queryByAltText('Logo de Test Agency Name')).toBeInTheDocument()
   })
 
   it('renders without a logo passed in', () => {
     const { queryByTestId } = render(<IdentifierMasthead />)
     expect(queryByTestId('identifierMasthead-logo')).not.toBeInTheDocument()
+  })
+
+  it('renders with more than two logos passed in', () => {
+    const { getAllByRole, queryByText } = render(
+      <IdentifierMasthead aria-label="Agency identifier">
+        <div className="usa-identifier__container">
+          <div className="usa-identifier__logos">
+            <IdentifierLogo agencyUrl="#">{testIdentifierLogo}</IdentifierLogo>
+            <IdentifierLogo agencyUrl="#">{testIdentifierLogo}</IdentifierLogo>
+            <IdentifierLogo agencyUrl="#">{testIdentifierLogo}</IdentifierLogo>
+          </div>
+          <div
+            data-testid="identifierMasthead-agency-description"
+            className="usa-identifier__identity"
+            aria-label="Agency description">
+            <p className="usa-identifier__identity-domain">
+              domain.edu.mil.gov
+            </p>
+            <p className="usa-identifier__identity-disclaimer">
+              {`An official website of the `}
+              <a href="testlink">Test Agency Name</a>
+              {`, `}
+              <a href="secondTestLink">Second Test Agency Name</a>
+              {`, and the `}
+              <a href="thirdTestLink">Third Test Agency Name</a>
+            </p>
+          </div>
+        </div>
+      </IdentifierMasthead>
+    )
+    expect(getAllByRole('img')).toHaveLength(3)
+    expect(getAllByRole('link')).toHaveLength(6)
+    expect(queryByText('Third Test Agency Name')).toBeInTheDocument()
   })
 })
