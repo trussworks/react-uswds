153d8c739ad54789ca2d3e4c576dcb188b2d4a23
Arianna Kellogg <ari@truss.works>
2021-04-05 16:34:42 -0700

    Add default Storybook example

--- src/components/SummaryBox/SummaryBox.stories.tsx
+++ src/components/SummaryBox/SummaryBox.stories.tsx
@@ -18,5 +18,53 @@ Source: https://designsystem.digital.gov/components/summary-box
 }
 
 export const SummaryBoxDefault = (): React.ReactElement => (
-  <SummaryBox heading="Example heading">some text</SummaryBox>
+  <SummaryBox heading="Key information">
+    <div className="usa-summary-box__text">
+      <ul className="usa-list">
+        <li>
+          If you are under a winter storm warning, &nbsp;
+          <a className="usa-summary-box__link" href="#usa-anchor-find-shelter">
+            find shelter
+          </a>
+          &nbsp; right away.
+        </li>
+        <li>
+          Sign up for &nbsp;
+          <a
+            className="usa-summary-box__link"
+            href="#usa-anchor-warning-system">
+            your community’s warning system
+          </a>
+          .
+        </li>
+        <li>
+          Learn the signs of, and basic treatments for, &nbsp;
+          <a className="usa-summary-box__link" href="#usa-anchor-frostbite">
+            frostbite
+          </a>
+          &nbsp; and &nbsp;
+          <a className="usa-summary-box__link" href="#usa-anchor-hypothermia">
+            hypothermia
+          </a>
+          .
+        </li>
+        <li>
+          Gather emergency supplies for your &nbsp;
+          <a className="usa-summary-box__link" href="#usa-anchor-home">
+            home
+          </a>
+          &nbsp; and your &nbsp;
+          <a className="usa-summary-box__link" href="#usa-anchor-car">
+            car
+          </a>
+          .
+        </li>
+      </ul>
+    </div>
+  </SummaryBox>
 )
+
+/*
+
+
+*/
--- src/components/SummaryBox/SummaryBox.tsx
+++ src/components/SummaryBox/SummaryBox.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import classnames from 'classnames'
+
 interface SummaryBoxProps {
   heading: string
   children?: React.ReactNode
@@ -8,11 +10,14 @@ interface SummaryBoxProps {
 export const SummaryBox = ({
   heading,
   children,
+  className,
   ...props
 }: SummaryBoxProps & JSX.IntrinsicElements['div']): React.ReactElement => {
+  const classes = classnames('usa-summary-box', className)
+  const bodyClasses = classnames('usa-summary-box__body')
   return (
-    <div className="usa-summary-box" {...props}>
-      <div className="usa-summary-box__body">
+    <div className={classes} role="complementary" {...props}>
+      <div className={bodyClasses}>
         <h3 className="usa-summary-box__heading">{heading}</h3>
         {children}
       </div>
