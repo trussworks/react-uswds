a2f40a0c727a4621909e1da610f5fd16e1bb38c6
mlm55 <mlm55@users.noreply.github.com>
2021-04-27 09:32:02 -0500

    feat: Radio Button Tile Variant (#1101)

--- src/components/forms/Radio/Radio.tsx
+++ src/components/forms/Radio/Radio.tsx
@@ -12,6 +12,8 @@ interface RadioProps {
     | React.RefObject<HTMLInputElement>
     | null
     | undefined
+  tile?: boolean
+  labelDescription?: React.ReactNode
 }
 
 export const Radio = ({
@@ -20,14 +22,19 @@ export const Radio = ({
   className,
   label,
   inputRef,
+  tile,
+  labelDescription,
   ...inputProps
 }: RadioProps & JSX.IntrinsicElements['input']): React.ReactElement => {
   const classes = classnames('usa-radio', className)
+  const radioClasses = classnames('usa-radio__input', {
+    'usa-radio__input--tile': tile,
+  })
 
   return (
     <div data-testid="radio" className={classes}>
       <input
-        className="usa-radio__input"
+        className={radioClasses}
         id={id}
         type="radio"
         name={name}
@@ -36,6 +43,11 @@ export const Radio = ({
       />
       <label className="usa-radio__label" htmlFor={id}>
         {label}
+        {labelDescription && (
+          <span className="usa-checkbox__label-description">
+            {labelDescription}
+          </span>
+        )}
       </label>
     </div>
   )
