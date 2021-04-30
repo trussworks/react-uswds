cd0a68fa06767ca8a83aeb3ef6bee7258dab7355
Arianna Kellogg <ari@truss.works>
2021-04-30 13:32:21 -0700

    feat: Update Grid components to render any type of element (#1166)
    * Update GridContainer to handle any component but default to a div, update tests, update storybook with customContainer story
    
    * Combine GridContainer examples in storybook
    
    * Update Grid component to render any type of element
    
    * Regenerate yarn.lock
    
    * Combine custom components into one Storybook example
    
    * Remove superfluous li element from custom storybook example
    
    * Remove unnecessary spaces from storybook file
    
    * Move call to classes function outside of if statement separating default from custom component creation
    
    * Update isCustomProps to accept "otherProps" (#1194)
    
    * Update isCustomProps to accept "otherProps"
    
    * Refactor out ommited props
    
    Co-authored-by: Brandon Lenz <brandonalenz@gmail.com>

--- src/components/grid/GridContainer/GridContainer.test.tsx
+++ src/components/grid/GridContainer/GridContainer.test.tsx
@@ -2,22 +2,89 @@ import React from 'react'
 import { render } from '@testing-library/react'
 
 import { GridContainer } from './GridContainer'
+import { Grid } from '../Grid/Grid'
+
+const testContent = 'a grid container item'
+const testGridContent = (
+  <Grid row>
+    <Grid tablet={{ col: true }}>{testContent}</Grid>
+    <Grid tablet={{ col: true }}>{testContent}</Grid>
+    <Grid tablet={{ col: true }}>{testContent}</Grid>
+  </Grid>
+)
 
 describe('GridContainer component', () => {
-  it('renders without errors', () => {
-    const { queryByTestId } = render(<GridContainer />)
-    expect(queryByTestId('gridContainer')).toBeInTheDocument()
+  describe('default component', () => {
+    it('renders without errors', () => {
+      const { queryByTestId } = render(
+        <GridContainer>{testGridContent}</GridContainer>
+      )
+      expect(queryByTestId('gridContainer')).toBeInTheDocument()
+    })
+
+    it('renders its children', () => {
+      const { queryByText } = render(<GridContainer>My Content</GridContainer>)
+      expect(queryByText('My Content')).toBeInTheDocument()
+    })
+
+    it('implements the containerSize prop', () => {
+      const { getByTestId } = render(
+        <GridContainer containerSize="tablet-lg">My Content</GridContainer>
+      )
+      expect(getByTestId('gridContainer')).toHaveClass(
+        'grid-container-tablet-lg'
+      )
+    })
   })
 
-  it('renders its children', () => {
-    const { queryByText } = render(<GridContainer>My Content</GridContainer>)
-    expect(queryByText('My Content')).toBeInTheDocument()
-  })
+  describe('custom component', () => {
+    type CustomGridContainerProps = JSX.IntrinsicElements['ul']
 
-  it('implements the containerSize prop', () => {
-    const { getByTestId } = render(
-      <GridContainer containerSize="tablet-lg">My Content</GridContainer>
+    const CustomGridContainer: React.FunctionComponent<CustomGridContainerProps> = ({
+      children,
+      className,
+      ...ulProps
+    }: CustomGridContainerProps): React.ReactElement => (
+      <ul className={className} {...ulProps}>
+        {children}
+      </ul>
     )
-    expect(getByTestId('gridContainer')).toHaveClass('grid-container-tablet-lg')
+
+    it('renders without errors', () => {
+      const { getByRole } = render(
+        <GridContainer<CustomGridContainerProps> asCustom={CustomGridContainer}>
+          <li>{testGridContent}</li>
+          <li>{testGridContent}</li>
+          <li>{testGridContent}</li>
+          <li>{testGridContent}</li>
+        </GridContainer>
+      )
+
+      const list = getByRole('list')
+      expect(list).toBeInTheDocument()
+      expect(list).toHaveClass('grid-container')
+    })
+
+    it('handles own props', () => {
+      const { getByRole, getByTestId } = render(
+        <GridContainer<CustomGridContainerProps>
+          asCustom={CustomGridContainer}
+          className="custom-class"
+          custom-attribute="customAtt"
+          data-testid="customTestId"
+          containerSize="mobile-lg">
+          <li>{testGridContent}</li>
+          <li>{testGridContent}</li>
+          <li>{testGridContent}</li>
+          <li>{testGridContent}</li>
+        </GridContainer>
+      )
+
+      const list = getByRole('list')
+      expect(list).toBeInTheDocument()
+      expect(list).toHaveAttribute('custom-attribute', 'customAtt')
+      expect(list).toHaveClass('grid-container-mobile-lg custom-class')
+      expect(getByTestId('customTestId')).toBeInTheDocument()
+    })
   })
 })
