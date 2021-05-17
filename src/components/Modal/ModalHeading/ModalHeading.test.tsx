b289aa4b049afa5fcd9355304e191ece603f67ce
Arianna Kellogg <ari@truss.works>
2021-05-17 13:28:36 -0700

    Skeleton for ModalHeading, passing one test

--- src/components/Modal/ModalHeading/ModalHeading.test.tsx
+++ src/components/Modal/ModalHeading/ModalHeading.test.tsx
@@ -1,1 +1,12 @@
 import React from 'react'
+import { render } from '@testing-library/react'
+
+import { ModalHeading } from './ModalHeading'
+
+describe('ModalHeading component', () => {
+  it('renders without errors', () => {
+    const { queryByTestId } = render(<ModalHeading />)
+
+    expect(queryByTestId('modalHeading')).toBeInTheDocument()
+  })
+})
--- src/components/Modal/ModalHeading/ModalHeading.tsx
+++ src/components/Modal/ModalHeading/ModalHeading.tsx
@@ -1,1 +1,7 @@
 import React from 'react'
+
+export const ModalHeading = (): React.ReactElement => {
+  return <div data-testid="modalHeading">modal heading</div>
+}
+
+export default ModalHeading
