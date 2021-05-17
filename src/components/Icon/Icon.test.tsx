291cdad6f44d7ac11abe1fead1413ab173157ba4
Arianna Kellogg <ari@truss.works>
2021-05-17 11:50:35 -0700

    Add tests for Icon component

--- src/components/Icon/Icon.test.tsx
+++ src/components/Icon/Icon.test.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 
-import { icons, IconKeys } from './icons'
 import { Icon } from './Icon'
 
 describe('Icon component', () => {
   it('renders without errors', () => {
-    const { queryByTestId } = render(<Icon name="add" size={8} />)
+    const { queryByTestId } = render(<Icon name="add" />)
+
     expect(queryByTestId('iconComponent')).toBeInTheDocument()
+  })
+
+  it('accepts a className', () => {
+    const { queryByRole } = render(
+      <Icon name="localLaundryService" className="custom-class" />
+    )
+
+    expect(queryByRole('img')).toHaveClass('custom-class')
+  })
+
+  it('accepts an aria-label', () => {
+    const { queryByRole } = render(
+      <Icon name="build" aria-label="build icon component" />
+    )
+
+    expect(queryByRole('img')).toHaveAttribute(
+      'aria-label',
+      'build icon component'
+    )
   })
 })
