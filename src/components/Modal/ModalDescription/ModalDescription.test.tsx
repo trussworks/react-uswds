a474a51147688346dad31d013381a1b571a6fc59
Arianna Kellogg <ari@truss.works>
2021-05-17 13:08:26 -0700

    Update ModalDeescription to include USWDS class, update tests

--- src/components/Modal/ModalDescription/ModalDescription.test.tsx
+++ src/components/Modal/ModalDescription/ModalDescription.test.tsx
@@ -5,8 +5,22 @@ import { ModalDescription } from './ModalDescription'
 
 describe('ModalDescription component', () => {
   it('renders without errors', () => {
-    const { queryByTestId } = render(<ModalDescription />)
+    const { queryByTestId } = render(
+      <ModalDescription>
+        You have unsaved changes that will be lost.
+      </ModalDescription>
+    )
 
     expect(queryByTestId('modalDescription')).toBeInTheDocument()
+  })
+
+  it('accepts a custom className', () => {
+    const { queryByTestId } = render(
+      <ModalDescription className="custom-class">
+        You have unsaved changes that will be lost.
+      </ModalDescription>
+    )
+
+    expect(queryByTestId('modalDescription')).toHaveClass('custom-class')
   })
 })
--- src/components/Modal/ModalDescription/ModalDescription.tsx
+++ src/components/Modal/ModalDescription/ModalDescription.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import classnames from 'classnames'
 
-export const ModalDescription = (): React.ReactElement => {
-  return <div data-testid="modalDescription">modal description</div>
+interface ModalDescriptionProps {
+  children: React.ReactNode
+  className?: string
+}
+
+export const ModalDescription = ({
+  children,
+  className,
+  ...divProps
+}: ModalDescriptionProps &
+  JSX.IntrinsicElements['div']): React.ReactElement => {
+  const classes = classnames('usa-modal__main', className)
+
+  return (
+    <div className={classes} data-testid="modalDescription" {...divProps}>
+      {children}
+    </div>
+  )
 }
 
 export default ModalDescription
