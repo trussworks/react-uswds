f5a40946fa5bf695bfd26991ffc83951eb262100
Arianna Kellogg <ari@truss.works>
2021-05-27 16:26:10 -0700

    Add tests for ability to pass custom attributes

--- src/components/IconList/IconList.test.tsx
+++ src/components/IconList/IconList.test.tsx
@@ -9,4 +9,22 @@ describe('IconList component', () => {
 
     expect(getAllByRole('listitem')).toHaveLength(3)
   })
+
+  it('accepts a className', () => {
+    const { queryByRole } = render(
+      <IconList className="custom-class">children</IconList>
+    )
+
+    expect(queryByRole('list')).toHaveClass('custom-class')
+  })
+
+  it('accepts attributes passed in through props', () => {
+    const { queryByRole } = render(
+      <IconList aria-label="icon list component">children</IconList>
+    )
+    expect(queryByRole('list')).toHaveAttribute(
+      'aria-label',
+      'icon list component'
+    )
+  })
 })
