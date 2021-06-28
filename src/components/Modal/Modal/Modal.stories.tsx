e633f9bcf59c0e09dd8de17f7d63416161b4a500
Arianna Kellogg <ari@truss.works>
2021-06-28 14:24:33 -0700

    Modal storybook update

--- src/components/Modal/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal/Modal.stories.tsx
@@ -30,77 +30,79 @@ Source: http://designsystem.digital.gov/components/modal
   },
 }
 
-export const modalWrapper = (): React.ReactElement => (
-  <ModalWrapper isVisible>some children</ModalWrapper>
-)
-
 export const defaultModal = (): React.ReactElement => (
-  <Modal>
-    <ModalContent>
-      <ModalMain>
-        <ModalHeading type="h2">
-          Are you sure you want to continue?
-        </ModalHeading>
-        <ModalDescription>
-          <p>You have unsaved changes that will be lost.</p>
-        </ModalDescription>
-        <ModalFooter>
-          <ButtonGroup>
-            <Button type="button">Continue without saving</Button>
-            <Button type="button">Go back</Button>
-          </ButtonGroup>
-        </ModalFooter>
-      </ModalMain>
-      <ModalCloseButton>
-        <img src={close} alt="Close this window" />
-      </ModalCloseButton>
-    </ModalContent>
-  </Modal>
+  <ModalWrapper isVisible>
+    <Modal>
+      <ModalContent>
+        <ModalMain>
+          <ModalHeading type="h2">
+            Are you sure you want to continue?
+          </ModalHeading>
+          <ModalDescription>
+            <p>You have unsaved changes that will be lost.</p>
+          </ModalDescription>
+          <ModalFooter>
+            <ButtonGroup>
+              <Button type="button">Continue without saving</Button>
+              <Button type="button">Go back</Button>
+            </ButtonGroup>
+          </ModalFooter>
+        </ModalMain>
+        <ModalCloseButton>
+          <img src={close} alt="Close this window" />
+        </ModalCloseButton>
+      </ModalContent>
+    </Modal>
+  </ModalWrapper>
 )
 
 // <p id="modal-1-description">You have unsaved changes that will be lost.</p>
 export const largeModal = (): React.ReactElement => (
-  <Modal isLarge>
-    large modal
-    <ModalContent>
-      <ModalMain>
-        <ModalHeading type="h2">
-          Are you sure you want to continue?
-        </ModalHeading>
-        <ModalDescription>
-          <p>You have unsaved changes that will be lost.</p>
-        </ModalDescription>
-        <ModalFooter>
-          <ButtonGroup>
-            <Button type="button">Continue without saving</Button>
-            <Button type="button">Go back</Button>
-          </ButtonGroup>
-        </ModalFooter>
-      </ModalMain>
-      <ModalCloseButton>
-        <img src={close} alt="Close this window" />
-      </ModalCloseButton>
-    </ModalContent>
-  </Modal>
+  <ModalWrapper isVisible>
+    <Modal isLarge>
+      large modal
+      <ModalContent>
+        <ModalMain>
+          <ModalHeading type="h2">
+            Are you sure you want to continue?
+          </ModalHeading>
+          <ModalDescription>
+            <p>You have unsaved changes that will be lost.</p>
+          </ModalDescription>
+          <ModalFooter>
+            <ButtonGroup>
+              <Button type="button">Continue without saving</Button>
+              <Button type="button">Go back</Button>
+            </ButtonGroup>
+          </ModalFooter>
+        </ModalMain>
+        <ModalCloseButton>
+          <img src={close} alt="Close this window" />
+        </ModalCloseButton>
+      </ModalContent>
+    </Modal>
+  </ModalWrapper>
 )
 
 export const modalWithForcedAction = (): React.ReactElement => (
-  <Modal>
-    <ModalContent>
-      <ModalMain>
-        <ModalHeading type="h2">
-          Are you sure you want to continue?
-        </ModalHeading>
-        <ModalDescription>
-          <p>You have unsaved changes that will be lost.</p>
-        </ModalDescription>
-        <ModalFooter>
-          <ButtonGroup>
-            <Button type="button">Continue without saving</Button>
-            <Button type="button">Go back</Button>
-          </ButtonGroup>
-        </ModalFooter>
-      </ModalMain>
-    </ModalContent>
-  </Modal>
+  <ModalWrapper isVisible>
+    <Modal>
+      <ModalContent>
+        <ModalMain>
+          <ModalHeading type="h2">
+            Are you sure you want to continue?
+          </ModalHeading>
+          <ModalDescription>
+            <p>You have unsaved changes that will be lost.</p>
+          </ModalDescription>
+          <ModalFooter>
+            <ButtonGroup>
+              <Button type="button">Continue without saving</Button>
+              <Button type="button">Go back</Button>
+            </ButtonGroup>
+          </ModalFooter>
+        </ModalMain>
+      </ModalContent>
+    </Modal>
+  </ModalWrapper>
 )
