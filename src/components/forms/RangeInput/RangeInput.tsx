00348350ced514bf33c9a1795270838e2df33a24
Kim Allen <kim@truss.works>
2021-09-20 09:12:29 -0700

    feat: Add aria attributes to RangeInput (#1560)
    * Add min and aria min label and tests
    
    * Add max values and tests
    
    * Use min value that corresponds to step in test
    
    * Apply aria value now based on current value
    
    * Fix value calculation and typing
    
    * Add .idea to .gitignore
    
    * Add option for aria- labels and tests
    	labels: aria-valuemin, aria-valuemax, aria-valuenow
    	tests for correct default value and passed in aria- labels
    
    * Fix default value formula
    
    * refer to aria props via snakecase - e.g. 'aria-valuenow' - strings rather than React camel case
    
    Co-authored-by: Hana Worku <hana@truss.works>

--- src/components/forms/RangeInput/RangeInput.tsx
+++ src/components/forms/RangeInput/RangeInput.tsx
@@ -4,6 +4,8 @@ import classnames from 'classnames'
 interface RangeInputProps {
   id: string
   name: string
+  min?: number
+  max?: number
   inputRef?:
     | string
     | ((instance: HTMLInputElement | null) => void)
@@ -17,9 +19,33 @@ export const RangeInput = ({
   inputRef,
   ...inputProps
 }: RangeInputProps & JSX.IntrinsicElements['input']): React.ReactElement => {
-  // Range defaults to min = 0, max = 100, step = 1, and value = (max/2) if not specified.
-
   const classes = classnames('usa-range', className)
+  // input range defaults to min = 0, max = 100, step = 1, and value = (max/2) if not specified.
+  const defaultMin = 0
+  const defaultMax = 100
+  const { min, max, defaultValue } = inputProps
+  const rangeMin = min || defaultMin
+  const rangeMax = max || defaultMax
+  const ariaMin = inputProps['aria-valuemin'] || rangeMin
+  const ariaMax = inputProps['aria-valuemax'] || rangeMax
+  const calculatedAriaValueNow =
+    inputProps['aria-valuenow'] ||
+    defaultValue ||
+    (rangeMax < rangeMin ? rangeMin : rangeMin + (rangeMax - rangeMin) / 2)
+  const convertValueType = (
+    value: string | number | readonly string[]
+  ): number | undefined => {
+    if (typeof value === 'number' || typeof value === 'string') {
+      return Number(value)
+    }
+    return undefined
+  }
+  const [ariaValue, setAriaValue] = React.useState<number | undefined>(
+    convertValueType(calculatedAriaValueNow)
+  )
+  const onValueChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (!inputProps['aria-valuenow']) setAriaValue(e.target.valueAsNumber)
+  }
 
   return (
     <input
@@ -28,6 +54,10 @@ export const RangeInput = ({
       ref={inputRef}
       type="range"
       {...inputProps}
+      aria-valuemin={ariaMin}
+      aria-valuemax={ariaMax}
+      aria-valuenow={ariaValue}
+      onChange={(e): void => onValueChange(e)}
     />
   )
 }
