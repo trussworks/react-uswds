0a862c1bfce8ba7eb012a1b3c9d6bbc5bf10fe8e
Jen Duong <duong.jenniferm@gmail.com>
2021-10-21 16:47:00 +0100

    feat: Add IconPrefix and IconSuffix components (#1687)
    Co-authored-by: jenbutongit <hi+git@jen.digital>

---
+++ src/components/forms/InputPrefix/InputPrefix.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import classnames from 'classnames'
+
+type InputPrefixProps = {
+  className?: string
+  children: React.ReactNode
+} & JSX.IntrinsicElements['div']
+
+export const InputPrefix = ({
+  className,
+  children,
+}: InputPrefixProps): React.ReactElement => {
+  const classes = classnames('usa-input-prefix', className)
+
+  return (
+    <div className={classes} aria-hidden="true" data-testid="InputPrefix">
+      {children}
+    </div>
+  )
+}
---
+++ src/components/forms/InputSuffix/InputSuffix.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import classnames from 'classnames'
+
+type InputSuffixProps = {
+  className?: string
+  children: React.ReactNode
+} & JSX.IntrinsicElements['div']
+
+export const InputSuffix = ({
+  className,
+  children,
+}: InputSuffixProps): React.ReactElement => {
+  const classes = classnames('usa-input-suffix', className)
+
+  return (
+    <div className={classes} aria-hidden="true" data-testid="InputSuffix">
+      {children}
+    </div>
+  )
+}
