0e914fef6a95529adb287a27d32e62e0b995080a
Isaac Garfinkle <isaac@truss.works>
2022-03-23 16:59:29 -0700

    Change SummaryBox to not create Heading and Content automatically

--- src/components/SummaryBox/SummaryBox/SummaryBox.tsx
+++ src/components/SummaryBox/SummaryBox/SummaryBox.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 
 import classnames from 'classnames'
-import { SummaryBoxContent } from '../SummaryBoxContent/SummaryBoxContent'
-import { SummaryBoxHeading } from '../SummaryBoxHeading/SummaryBoxHeading'
 
 interface SummaryBoxProps {
-  heading: string
   children?: React.ReactNode
   className?: string
 }
 
 export const SummaryBox = ({
-  heading,
   children,
   className,
   ...divProps
@@ -19,10 +15,7 @@ export const SummaryBox = ({
   const classes = classnames('usa-summary-box', className)
   return (
     <div className={classes} data-testid="summary-box" {...divProps}>
-      <div className="usa-summary-box__body">
-        <SummaryBoxHeading heading={heading} />
-        <SummaryBoxContent>{children}</SummaryBoxContent>
-      </div>
+      <div className="usa-summary-box__body">{children}</div>
     </div>
   )
 }
