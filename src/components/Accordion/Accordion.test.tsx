caf3c0ae857592ba48ad15c1d04103098df7bca3
Isaac Garfinkle <isaac@truss.works>
2022-04-07 11:15:35 -0700

    factor out test text

--- src/components/Accordion/Accordion.test.tsx
+++ src/components/Accordion/Accordion.test.tsx
@@ -5,83 +5,86 @@ import { Accordion, AccordionItemProps } from './Accordion'
 import { deprecationWarning } from '../../deprecation'
 jest.mock('../../deprecation')
 
+const firstAmendment = (
+  <p>
+    Congress shall make no law respecting an establishment of religion, or
+    prohibiting the free exercise thereof; or abridging the freedom of speech,
+    or of the press; or the right of the people peaceably to assemble, and to
+    petition the Government for a redress of grievances.
+  </p>
+)
+const secondAmendment = (
+  <>
+    <p>
+      A well regulated Militia, being necessary to the security of a free State,
+      the right of the people to keep and bear Arms, shall not be infringed.
+    </p>{' '}
+    <ul>
+      <li>This is a list item</li>
+      <li>Another list item</li>
+    </ul>
+  </>
+)
+const thirdAmendment = (
+  <p>
+    No Soldier shall, in time of peace be quartered in any house, without the
+    consent of the Owner, nor in time of war, but in a manner to be prescribed
+    by law.
+  </p>
+)
+const fourthAmendment = (
+  <p>
+    The right of the people to be secure in their persons, houses, papers, and
+    effects, against unreasonable searches and seizures, shall not be violated,
+    and no Warrants shall issue, but upon probable cause, supported by Oath or
+    affirmation, and particularly describing the place to be searched, and the
+    persons or things to be seized.
+  </p>
+)
+const fifthAmendment = (
+  <p>
+    No person shall be held to answer for a capital, or otherwise infamous
+    crime, unless on a presentment or indictment of a Grand Jury, except in
+    cases arising in the land or naval forces, or in the Militia, when in actual
+    service in time of War or public danger; nor shall any person be subject for
+    the same offence to be twice put in jeopardy of life or limb; nor shall be
+    compelled in any criminal case to be a witness against himself, nor be
+    deprived of life, liberty, or property, without due process of law; nor
+    shall private property be taken for public use, without just compensation.
+  </p>
+)
 const testItems: AccordionItemProps[] = [
   {
     title: 'First Amendment',
-    content: (
-      <p>
-        Congress shall make no law respecting an establishment of religion, or
-        prohibiting the free exercise thereof; or abridging the freedom of
-        speech, or of the press; or the right of the people peaceably to
-        assemble, and to petition the Government for a redress of grievances.
-      </p>
-    ),
+    content: firstAmendment,
     expanded: false,
     id: '123',
     headingLevel: 'h4',
   },
   {
     title: 'Second Amendment',
-    content: (
-      <>
-        <p>
-          A well regulated Militia, being necessary to the security of a free
-          State, the right of the people to keep and bear Arms, shall not be
-          infringed.
-        </p>{' '}
-        <ul>
-          <li>This is a list item</li>
-          <li>Another list item</li>
-        </ul>
-      </>
-    ),
+    content: secondAmendment,
     expanded: false,
     id: 'abc',
     headingLevel: 'h4',
   },
   {
     title: 'Third Amendment',
-    content: (
-      <p>
-        No Soldier shall, in time of peace be quartered in any house, without
-        the consent of the Owner, nor in time of war, but in a manner to be
-        prescribed by law.
-      </p>
-    ),
+    content: thirdAmendment,
     expanded: false,
     id: 'def',
     headingLevel: 'h4',
   },
   {
     title: 'Fourth Amendment',
-    content: (
-      <p>
-        The right of the people to be secure in their persons, houses, papers,
-        and effects, against unreasonable searches and seizures, shall not be
-        violated, and no Warrants shall issue, but upon probable cause,
-        supported by Oath or affirmation, and particularly describing the place
-        to be searched, and the persons or things to be seized.
-      </p>
-    ),
+    content: fourthAmendment,
     expanded: false,
     id: '456',
     headingLevel: 'h4',
   },
   {
     title: 'Fifth Amendment',
-    content: (
-      <p>
-        No person shall be held to answer for a capital, or otherwise infamous
-        crime, unless on a presentment or indictment of a Grand Jury, except in
-        cases arising in the land or naval forces, or in the Militia, when in
-        actual service in time of War or public danger; nor shall any person be
-        subject for the same offence to be twice put in jeopardy of life or
-        limb; nor shall be compelled in any criminal case to be a witness
-        against himself, nor be deprived of life, liberty, or property, without
-        due process of law; nor shall private property be taken for public use,
-        without just compensation.
-      </p>
-    ),
+    content: fifthAmendment,
     expanded: false,
     id: '789',
     headingLevel: 'h4',
@@ -237,81 +240,34 @@ describe('Accordion component', () => {
     const testExpandedItems: AccordionItemProps[] = [
       {
         title: 'First Amendment',
-        content: (
-          <p>
-            Congress shall make no law respecting an establishment of religion,
-            or prohibiting the free exercise thereof; or abridging the freedom
-            of speech, or of the press; or the right of the people peaceably to
-            assemble, and to petition the Government for a redress of
-            grievances.
-          </p>
-        ),
+        content: firstAmendment,
         expanded: false,
         id: '123',
         headingLevel: 'h4',
       },
       {
         title: 'Second Amendment',
-        content: (
-          <>
-            <p>
-              A well regulated Militia, being necessary to the security of a
-              free State, the right of the people to keep and bear Arms, shall
-              not be infringed.
-            </p>{' '}
-            <ul>
-              <li>This is a list item</li>
-              <li>Another list item</li>
-            </ul>
-          </>
-        ),
+        content: secondAmendment,
         expanded: true,
         id: 'abc',
         headingLevel: 'h4',
       },
       {
         title: 'Third Amendment',
-        content: (
-          <p>
-            No Soldier shall, in time of peace be quartered in any house,
-            without the consent of the Owner, nor in time of war, but in a
-            manner to be prescribed by law.
-          </p>
-        ),
+        content: thirdAmendment,
         expanded: false,
         id: 'def',
       },
       {
         title: 'Fourth Amendment',
-        content: (
-          <p>
-            The right of the people to be secure in their persons, houses,
-            papers, and effects, against unreasonable searches and seizures,
-            shall not be violated, and no Warrants shall issue, but upon
-            probable cause, supported by Oath or affirmation, and particularly
-            describing the place to be searched, and the persons or things to be
-            seized.
-          </p>
-        ),
+        content: fourthAmendment,
         expanded: true,
         id: '456',
         headingLevel: 'h4',
       },
       {
         title: 'Fifth Amendment',
-        content: (
-          <p>
-            No person shall be held to answer for a capital, or otherwise
-            infamous crime, unless on a presentment or indictment of a Grand
-            Jury, except in cases arising in the land or naval forces, or in the
-            Militia, when in actual service in time of War or public danger; nor
-            shall any person be subject for the same offence to be twice put in
-            jeopardy of life or limb; nor shall be compelled in any criminal
-            case to be a witness against himself, nor be deprived of life,
-            liberty, or property, without due process of law; nor shall private
-            property be taken for public use, without just compensation.
-          </p>
-        ),
+        content: fifthAmendment,
         expanded: true,
         id: '789',
         headingLevel: 'h4',
@@ -342,15 +298,7 @@ describe('Accordion component', () => {
     const customTestItems: AccordionItemProps[] = [
       {
         title: 'First Amendment',
-        content: (
-          <p>
-            Congress shall make no law respecting an establishment of religion,
-            or prohibiting the free exercise thereof; or abridging the freedom
-            of speech, or of the press; or the right of the people peaceably to
-            assemble, and to petition the Government for a redress of
-            grievances.
-          </p>
-        ),
+        content: firstAmendment,
         expanded: false,
         id: '123',
         className: 'myCustomAccordionItem',
@@ -358,32 +306,14 @@ describe('Accordion component', () => {
       },
       {
         title: 'Second Amendment',
-        content: (
-          <>
-            <p>
-              A well regulated Militia, being necessary to the security of a
-              free State, the right of the people to keep and bear Arms, shall
-              not be infringed.
-            </p>{' '}
-            <ul>
-              <li>This is a list item</li>
-              <li>Another list item</li>
-            </ul>
-          </>
-        ),
+        content: secondAmendment,
         expanded: false,
         id: 'abc',
         headingLevel: 'h4',
       },
       {
         title: 'Third Amendment',
-        content: (
-          <p>
-            No Soldier shall, in time of peace be quartered in any house,
-            without the consent of the Owner, nor in time of war, but in a
-            manner to be prescribed by law.
-          </p>
-        ),
+        content: thirdAmendment,
         expanded: false,
         id: 'def',
         headingLevel: 'h4',
@@ -402,15 +332,7 @@ describe('Accordion component', () => {
       const customTestItems: AccordionItemProps[] = [
         {
           title: 'First Amendment',
-          content: (
-            <p>
-              Congress shall make no law respecting an establishment of
-              religion, or prohibiting the free exercise thereof; or abridging
-              the freedom of speech, or of the press; or the right of the people
-              peaceably to assemble, and to petition the Government for a
-              redress of grievances.
-            </p>
-          ),
+          content: firstAmendment,
           expanded: false,
           headingLevel: 'h2',
           id: '123',
@@ -418,19 +340,7 @@ describe('Accordion component', () => {
         },
         {
           title: 'Second Amendment',
-          content: (
-            <>
-              <p>
-                A well regulated Militia, being necessary to the security of a
-                free State, the right of the people to keep and bear Arms, shall
-                not be infringed.
-              </p>{' '}
-              <ul>
-                <li>This is a list item</li>
-                <li>Another list item</li>
-              </ul>
-            </>
-          ),
+          content: secondAmendment,
           expanded: false,
           headingLevel: 'h2',
 
@@ -438,13 +348,7 @@ describe('Accordion component', () => {
         },
         {
           title: 'Third Amendment',
-          content: (
-            <p>
-              No Soldier shall, in time of peace be quartered in any house,
-              without the consent of the Owner, nor in time of war, but in a
-              manner to be prescribed by law.
-            </p>
-          ),
+          content: thirdAmendment,
           expanded: false,
           headingLevel: 'h2',
           id: 'def',
@@ -470,46 +374,20 @@ describe('Accordion component', () => {
       const customTestItems: AccordionItemProps[] = [
         {
           title: 'First Amendment',
-          content: (
-            <p>
-              Congress shall make no law respecting an establishment of
-              religion, or prohibiting the free exercise thereof; or abridging
-              the freedom of speech, or of the press; or the right of the people
-              peaceably to assemble, and to petition the Government for a
-              redress of grievances.
-            </p>
-          ),
+          content: firstAmendment,
           expanded: false,
           id: '123',
           className: 'myCustomAccordionItem',
         },
         {
           title: 'Second Amendment',
-          content: (
-            <>
-              <p>
-                A well regulated Militia, being necessary to the security of a
-                free State, the right of the people to keep and bear Arms, shall
-                not be infringed.
-              </p>{' '}
-              <ul>
-                <li>This is a list item</li>
-                <li>Another list item</li>
-              </ul>
-            </>
-          ),
+          content: secondAmendment,
           expanded: false,
           id: 'abc',
         },
         {
           title: 'Third Amendment',
-          content: (
-            <p>
-              No Soldier shall, in time of peace be quartered in any house,
-              without the consent of the Owner, nor in time of war, but in a
-              manner to be prescribed by law.
-            </p>
-          ),
+          content: thirdAmendment,
           expanded: false,
           id: 'def',
         },
