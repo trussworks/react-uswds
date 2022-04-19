63d3dd71d021e87047510fb7182e850036cf99c1
Henric Trotzig <henric.trotzig@gmail.com>
2022-04-19 09:08:57 +0200

    Upgrade happo.io library from 6.8.0=>7.1.0
    I noticed that you had a compareThreshold configured in .happo.js. This
    option is not in effect since version 6.8.0. It is replaced by more
    fine-grained threshold options configured through the happo.io UI. See
    docs here:
    https://docs.happo.io/docs/compare-threshold
    
    I've updated settings on your account to roughly match the previous
    compareThreshold. If you are an admin of the Happo account, you can see
    and edit those changes here:
    https://happo.io/a/371/projects/453/thresholds
    
    Below are Release Notes for the versions of happo.io that are part of
    the 6.8.0 to 7.1.0 upgrade.
    
    v7.1.0
    https://github.com/happo/happo.io/releases/tag/v7.1.0
    
    A new CI script targeted at Azure Pipelines has been added:
    happo-ci-azure-pipelines
    Dependencies updated to their latest patch version.
    
    v7.0.2
    https://github.com/happo/happo.io/releases/tag/v7.0.2
    
    Fix requiring react-dom in windows environments.
    
    v7.0.1
    https://github.com/happo/happo.io/releases/tag/v7.0.1
    
    This release contains a bugfix for running in async mode (the default,
    or when HAPPO_IS_ASYNC=true). The environment variable wasn't made
    available to happo run/happo compare sub commands in certain shell
    environments.
    
    v7.0.0
    https://github.com/happo/happo.io/releases/tag/v7.0.0
    
    Breaking changes:
    
    Drop support for Node v10
    Drop support for Webpack v3 (things should still work, but you might
    have to tweak some webpack config)
    Custom functions in jsdomOptions are now merged with defaults (instead
    of just overridden). If you have a beforeParse function configured, both
    the default beforeParse and your custom function will be called. Thanks
    to @konstrybakov!
    The env variable HAPPO_IS_ASYNC, which you can use to control whether or
    not to wait for screenshot results in CI, is now "true" by default. If
    you rely on a comment posted with the Happo result, you need to set
    HAPPO_IS_ASYNC=false before starting your happo run.
    
    New features and other improvements:
    Support for static happo runs where you bundle your happo test run
    yourself. See https://github.com/happo/happo-static for more info.
    Check with happo.io server first to see if assets/static package needs
    to be uploaded.
    
    Chores:
    Replace deprecated request library

--- .happo.js
+++ .happo.js
@@ -6,7 +6,6 @@ require('dotenv').config()
 module.exports = {
   apiKey: process.env.HAPPO_API_KEY,
   apiSecret: process.env.HAPPO_API_SECRET,
-  compareThreshold: 0.375,
   targets: {
     chrome: new RemoteBrowserTarget('chrome', {
       viewport: '1024x768',
