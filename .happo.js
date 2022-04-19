f11378617871afb7a91050822547898516f3d3bb
Isaac Garfinkle <isaac@truss.works>
2022-04-19 16:06:54 -0700

    stop checking happo on IE11

--- .happo.js
+++ .happo.js
@@ -16,10 +16,6 @@ module.exports = {
     edge: new RemoteBrowserTarget('edge', {
       viewport: '1024x768',
     }),
-    'internet explorer': new RemoteBrowserTarget('internet explorer', {
-      viewport: '1024x768',
-      chunks: 3,
-    }),
     safari: new RemoteBrowserTarget('safari', {
       viewport: '1024x768',
     }),
