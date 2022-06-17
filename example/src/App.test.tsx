1eb579e4d18e3ee04d3f54fc3445e6539fce3ec6
haworku <haworku@users.noreply.github.com>
2022-06-17 12:43:52 -0500

    feat: Remove example app (#2162)
    * Delete the /example folder
    
    * Remove references to example app
    
    * Cleanup
    
    Co-authored-by: Roger Steve Ruiz <rogeruiz@truss.works>

--- example/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-// TODO Add additional app testing
-test('renders the app without error', () => {
-  const { getByText } = render(<App />)
-  expect(getByText('Examples')).toBeInTheDocument()
-})
