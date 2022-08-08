9ff2f6285a91455b9cb10f7b0e1fc085a1331442
Brandon Lenz <brandon@truss.works>
2022-08-08 17:37:00 -0400

    Add BannerHeader tests

--- src/components/banner/BannerHeader/BannerHeader.tsx
+++ src/components/banner/BannerHeader/BannerHeader.tsx
@@ -4,13 +4,10 @@ import classNames from 'classnames'
 type BannerHeaderProps = {
   isOpen: boolean
   flagImg: ReactNode
-  innerDivClassName?: string
   innerDivProps?: JSX.IntrinsicElements['div']
   headerText: ReactNode
-  headerTextClassName?: string
   headerTextProps?: JSX.IntrinsicElements['p']
   headerActionText: ReactNode
-  headerActionClassName?: string
   headerActionProps?: JSX.IntrinsicElements['p']
 }
 
@@ -18,43 +15,61 @@ export const BannerHeader = ({
   children,
   isOpen,
   flagImg,
-  innerDivClassName,
   innerDivProps,
   headerText,
-  headerTextClassName,
   headerTextProps,
   headerActionText,
-  headerActionClassName,
   headerActionProps,
   className,
   ...headerProps
 }: BannerHeaderProps & JSX.IntrinsicElements['header']): ReactElement => {
-  const headerClasses = classNames('usa-banner__header', {
-    'usa-banner__header--expanded': isOpen,
-    className,
-  })
+  const classes = classNames(
+    'usa-banner__header',
+    {
+      'usa-banner__header--expanded': isOpen,
+    },
+    className
+  )
+
+  const { className: innerDivClassName, ...remainingInnerDivProps } =
+    innerDivProps || {}
   const innerDivClasses = classNames('usa-banner__inner', innerDivClassName)
+
+  const { className: headerTextClassName, ...remainingHeaderTextProps } =
+    headerTextProps || {}
   const headerTextClasses = classNames(
     'usa-banner__header-text',
     headerTextClassName
   )
+
+  const { className: headerActionClassName, ...remainingHeaderActionProps } =
+    headerActionProps || {}
   const headerActionClasses = classNames(
     'usa-banner__header-action',
     headerActionClassName
   )
 
   return (
-    <header className={headerClasses} {...headerProps}>
-      <div className={innerDivClasses} {...innerDivProps}>
-        {flagImg && <div className="grid-col-auto">{flagImg}</div>}
-        <div className="grid-col-fill tablet:grid-col-auto">
-          <p className={headerTextClasses} {...headerTextProps}>
+    <header className={classes} {...headerProps}>
+      <div
+        className={innerDivClasses}
+        {...remainingInnerDivProps}
+        data-testid="banner-header-inner-div">
+        {flagImg && (
+          <div className="grid-col-auto" data-testid="banner-header-flag-div">
+            {flagImg}
+          </div>
+        )}
+        <div
+          className="grid-col-fill tablet:grid-col-auto"
+          data-testid="banner-header-grid-div">
+          <p className={headerTextClasses} {...remainingHeaderTextProps}>
             {headerText}
           </p>
           <p
             className={headerActionClasses}
             aria-hidden="true"
-            {...headerActionProps}>
+            {...remainingHeaderActionProps}>
             {headerActionText}
           </p>
         </div>
