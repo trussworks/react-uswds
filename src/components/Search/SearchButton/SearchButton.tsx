13b49462f9b8651c65a1ebce048ebbfb3e4a10f0
Pearl Truss <pearl@truss.works>
2022-12-09 15:39:28 -0500

    created SearchButton component

--- src/components/Search/SearchButton/SearchButton.tsx
+++ src/components/Search/SearchButton/SearchButton.tsx
@@ -1,85 +1,37 @@
 import React from 'react'
-import classnames from 'classnames'
 
 import searchImg from '@uswds/uswds/src/img/usa-icons-bg/search--white.svg'
 
 import { Button } from '../../Button/Button'
-import { Form, OptionalFormProps } from '../../forms/Form/Form'
-import { SearchField } from '../SearchField/SearchField'
-import { OptionalTextInputProps } from '../../forms/TextInput/TextInput'
-import { Label } from '../../forms/Label/Label'
 
 type SearchLocalization = {
   buttonText: string
 }
 
-type SearchInputProps = {
-  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void
+type SearchButtonProps = {
   size?: 'big' | 'small'
-  className?: string
-  inputName?: string
-  inputId?: string
-  placeholder?: string
-  label?: React.ReactNode
   i18n?: SearchLocalization
-  inputProps?: OptionalTextInputProps
 }
 
-export const Search = ({
-  onSubmit,
+export const SearchButton = ({
   size,
-  className,
-  placeholder,
-  inputName = 'search',
-  label = 'Search',
-  inputId = 'search-field',
-  i18n,
-  inputProps,
-  ...formProps
-}: SearchInputProps & OptionalFormProps): React.ReactElement => {
+  i18n
+}: SearchButtonProps): React.ReactElement => {
   const buttonText = i18n?.buttonText || 'Search'
-
-  const isBig = size === 'big'
   const isSmall = size === 'small'
-  const classes = classnames(
-    'usa-search',
-    {
-      'usa-search--small': isSmall,
-      'usa-search--big': isBig,
-    },
-    className
-  )
 
   return (
-    <Form
-      onSubmit={onSubmit}
-      className={classes}
-      role="search"
-      search={true}
-      {...formProps}>
-      <Label srOnly={true} htmlFor={inputId}>
-        {label}
-      </Label>
-      <SearchField 
-        {...inputProps}
-        inputId={inputId}
-        placeholder={placeholder}
-        name={inputName}
-        defaultValue={formProps.defaultValue}
-        label={<p>hello</p>}
-      />
-      <Button type="submit">
+    <Button type="submit">
         {!isSmall && (
-          <span className="usa-search__submit-text">{buttonText}</span>
+            <span className="usa-search__submit-text">{buttonText}</span>
         )}
         <img
-          src={searchImg}
-          className="usa-search__submit-icon"
-          alt={buttonText}
+            src={searchImg}
+            className="usa-search__submit-icon"
+            alt={buttonText}
         />
-      </Button>
-    </Form>
+    </Button>
   )
 }
 
-export default Search
+export default SearchButton
