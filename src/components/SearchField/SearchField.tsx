98399ddf3cf1a4273d816076d16ca57157156c33
Pearl Truss <pearl@truss.works>
2022-12-09 11:03:48 -0500

    update SearchField to have correct props, structure

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,58 +1,42 @@
 import React from 'react'
-import classnames from 'classnames'
 
-import { OptionalFormProps } from '../forms/Form/Form'
 import { FormGroup } from '../forms/FormGroup/FormGroup'
 
 import { Label } from '../forms/Label/Label'
-import { TextInput } from '../forms/TextInput/TextInput'
+import { TextInput, OptionalTextInputProps } from '../forms/TextInput/TextInput'
 
-type SearchLocalization = {
-  buttonText: string
-}
 
 type SearchInputProps = {
-  size?: 'big' | 'small'
   className?: string
   inputName?: string
   inputId?: string
   placeholder?: string
+  defaultValue?: string
   label?: React.ReactNode
-  i18n?: SearchLocalization
 }
 
 export const Search = ({
-  size,
   className,
   placeholder,
+  defaultValue,
   inputName = 'search',
   label = 'Search',
   inputId = 'search-field',
-  ...formProps
-}: SearchInputProps & OptionalFormProps): React.ReactElement => {
-
-  // const isBig = size === 'big'
-  // const isSmall = size === 'small'
-  // const classes = classnames(
-  //   'usa-search',
-  //   {
-  //     'usa-search--small': isSmall,
-  //     'usa-search--big': isBig,
-  //   },
-  //   className
-  // )
+  ...inputProps
+}: SearchInputProps & OptionalTextInputProps): React.ReactElement => {
 
   return (
-    <FormGroup>
+    <FormGroup className={className}>
       <Label srOnly={true} htmlFor={inputId}>
         {label}
       </Label>
       <TextInput
+        {...inputProps}
         id={inputId}
-        type="search"
         name={inputName}
+        type="search"
         placeholder={placeholder}
-        defaultValue={formProps.defaultValue}
+        defaultValue={defaultValue}
       />
     </FormGroup>
   )
