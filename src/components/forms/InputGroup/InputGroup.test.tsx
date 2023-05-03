61e9535b6bdca1550b9a716552116ad9fdc200f2
Jacob Capps <jcapps@truss.works>
2023-05-03 15:44:58 -0500

    Add tests to verify classNames are applied

--- src/components/forms/InputGroup/InputGroup.test.tsx
+++ src/components/forms/InputGroup/InputGroup.test.tsx
@@ -13,4 +13,18 @@ describe('InputGroup component', () => {
     const { queryByText } = render(<InputGroup>My Input Group</InputGroup>)
     expect(queryByText('My Input Group')).toBeInTheDocument()
   })
+
+  it('adds the error class when error is true', () => {
+    const { queryByTestId } = render(
+      <InputGroup error>My Input Group</InputGroup>
+    )
+    expect(queryByTestId('inputGroup')).toHaveClass('usa-input-group--error')
+  })
+
+  it('adds the provided className', () => {
+    const { queryByTestId } = render(
+      <InputGroup className="my-class">My Input Group</InputGroup>
+    )
+    expect(queryByTestId('inputGroup')).toHaveClass('my-class')
+  })
 })
