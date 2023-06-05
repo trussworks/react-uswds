9a6c55d53a9f07082454c29376be8f0b54928dd9
andy <andy@andyhub.com>
2023-06-05 12:18:08 -0700

    moved stories from last merge

--- src/components/forms/Dropdown/Dropdown.stories.tsx
+++ src/components/forms/Dropdown/Dropdown.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { Dropdown } from './Dropdown'
 import { Label } from '../Label/Label'
-import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
   title: 'Components/Dropdown',
@@ -20,50 +19,43 @@ Source: https://designsystem.digital.gov/components/dropdown/
       },
     },
   },
-  argTypes: {
-    validationStatus: {
-      options: ['error', 'success'],
-      control: 'radio',
-    },
-    disabled: { control: 'boolean' },
-  },
-} as ComponentMeta<typeof Dropdown>
+}
 
-const options = (
-  <>
+export const defaultDropdown = (): React.ReactElement => (
+  <Dropdown id="input-dropdown" name="input-dropdown">
     <option>- Select - </option>
     <option value="value1">Option A</option>
     <option value="value2">Option B</option>
     <option value="value3">Option C</option>
-  </>
+  </Dropdown>
 )
 
-const Template: ComponentStory<typeof Dropdown> = (args) => (
-  <Dropdown {...args}>{options}</Dropdown>
+export const withDefaultValue = (): React.ReactElement => (
+  <Dropdown id="input-dropdown" name="input-dropdown" defaultValue="value2">
+    <option>- Select - </option>
+    <option value="value1">Option A</option>
+    <option value="value2">Option B</option>
+    <option value="value3">Option C</option>
+  </Dropdown>
 )
 
-export const Default = Template.bind({})
-Default.args = { id: 'input-dropdown', name: 'input-dropdown' }
-
-export const WithDefaultValue = Template.bind({})
-WithDefaultValue.args = {
-  id: 'input-dropdown',
-  name: 'input-dropdown',
-  defaultValue: 'value2',
-}
-
-export const Disabled = Template.bind({})
-Disabled.args = {
-  id: 'input-dropdown',
-  name: 'input-dropdown',
-  disabled: true,
-}
-
-export const WithLabel = () => (
+export const withLabel = (): React.ReactElement => (
   <>
-    <Label htmlFor="input-dropdown">Dropdown label</Label>
+    <Label htmlFor="options">Dropdown label</Label>
     <Dropdown id="input-dropdown" name="input-dropdown">
-      {options}
+      <option>- Select - </option>
+      <option value="value1">Option A</option>
+      <option value="value2">Option B</option>
+      <option value="value3">Option C</option>
     </Dropdown>
   </>
+)
+
+export const disabled = (): React.ReactElement => (
+  <Dropdown id="input-dropdown" name="input-dropdown" disabled>
+    <option>- Select - </option>
+    <option value="value1">Option A</option>
+    <option value="value2">Option B</option>
+    <option value="value3">Option C</option>
+  </Dropdown>
 )
--- src/components/forms/Select/Select.stories.tsx
+++ src/components/forms/Select/Select.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Select } from './Select'
 import { Label } from '../Label/Label'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
   title: 'Components/Select',
@@ -10,50 +11,57 @@ export default {
     docs: {
       description: {
         component: `
-### USWDS 3.0 Select component
+        ### USWDS 3.0 Select component
 
-Source: https://designsystem.digital.gov/components/select/
-`,
+        Source: https://designsystem.digital.gov/components/select/
+        `,
       },
     },
   },
-}
+  argTypes: {
+    validationStatus: {
+      options: ['error', 'success'],
+      control: 'radio',
+    },
+    disabled: { control: 'boolean' },
+  },
+} as ComponentMeta<typeof Select>
 
-export const defaultSelect = (): React.ReactElement => (
-  <Select id="input-select" name="input-select">
-    <option>- Select - </option>
-    <option value="value1">Option A</option>
-    <option value="value2">Option B</option>
-    <option value="value3">Option C</option>
-  </Select>
-)
-
-export const withDefaultValue = (): React.ReactElement => (
-  <Select id="input-select" name="input-select" defaultValue="value2">
-    <option>- Select - </option>
-    <option value="value1">Option A</option>
-    <option value="value2">Option B</option>
-    <option value="value3">Option C</option>
-  </Select>
-)
-
-export const withLabel = (): React.ReactElement => (
+const options = (
   <>
-    <Label htmlFor="options">Select label</Label>
-    <Select id="input-select" name="input-select">
-      <option>- Select - </option>
-      <option value="value1">Option A</option>
-      <option value="value2">Option B</option>
-      <option value="value3">Option C</option>
-    </Select>
+    <option>- Select - </option>
+    <option value="value1">Option A</option>
+    <option value="value2">Option B</option>
+    <option value="value3">Option C</option>
   </>
 )
 
-export const disabled = (): React.ReactElement => (
-  <Select id="input-select" name="input-select" disabled>
-    <option>- Select - </option>
-    <option value="value1">Option A</option>
-    <option value="value2">Option B</option>
-    <option value="value3">Option C</option>
-  </Select>
+const Template: ComponentStory<typeof Select> = (args) => (
+  <Select {...args}>{options}</Select>
+)
+
+export const Default = Template.bind({})
+Default.args = { id: 'input-select', name: 'input-select' }
+
+export const WithDefaultValue = Template.bind({})
+WithDefaultValue.args = {
+  id: 'input-select',
+  name: 'input-select',
+  defaultValue: 'value2',
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  id: 'input-select',
+  name: 'input-select',
+  disabled: true,
+}
+
+export const WithLabel = () => (
+  <>
+    <Label htmlFor="input-select">Select label</Label>
+    <Select id="input-select" name="input-select">
+      {options}
+    </Select>
+  </>
 )
