75e936985cc1cd2f2052f902f0806c58446aca30
Andrew Nelson <andy@andyhub.com>
2023-06-06 09:42:32 -0700

    fix: Creates new Select component to eventually replace Dropdown (#2415)
    Co-authored-by: Brandon Lenz <brandon@truss.works>

--- src/components/forms/Dropdown/Dropdown.stories.tsx
+++ src/components/forms/Dropdown/Dropdown.stories.tsx
@@ -2,66 +2,60 @@ import React from 'react'
 
 import { Dropdown } from './Dropdown'
 import { Label } from '../Label/Label'
-import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
   title: 'Components/Dropdown',
-  component: Dropdown,
+  component: 'Dropdown',
   parameters: {
     docs: {
       description: {
         component: `
-### USWDS 3.0 Dropdown component
+### Deprecated USWDS 2.x Dropdown component
 
-Source: https://designsystem.digital.gov/components/select/
+⚠️ Dropdown is deprecated and will be removed in the future. Please use the Select component instead.
+
+Source: https://designsystem.digital.gov/components/dropdown/
 `,
       },
     },
   },
-  argTypes: {
-    validationStatus: {
-      options: ['error', 'success'],
-      control: 'radio',
-    },
-    disabled: { control: 'boolean' },
-  },
-} as ComponentMeta<typeof Dropdown>
+}
 
-const options = (
-  <>
+export const defaultDropdown = (): React.ReactElement => (
+  <Dropdown id="input-dropdown" name="input-dropdown">
     <option>- Select - </option>
     <option value="value1">Option A</option>
     <option value="value2">Option B</option>
     <option value="value3">Option C</option>
-  </>
+  </Dropdown>
 )
 
-const Template: ComponentStory<typeof Dropdown> = (args) => (
-  <Dropdown {...args}>{options}</Dropdown>
+export const withDefaultValue = (): React.ReactElement => (
+  <Dropdown id="input-dropdown" name="input-dropdown" defaultValue="value2">
+    <option>- Select - </option>
+    <option value="value1">Option A</option>
+    <option value="value2">Option B</option>
+    <option value="value3">Option C</option>
+  </Dropdown>
 )
 
-export const Default = Template.bind({})
-Default.args = { id: 'input-dropdown', name: 'input-dropdown' }
-
-export const WithDefaultValue = Template.bind({})
-WithDefaultValue.args = {
-  id: 'input-dropdown',
-  name: 'input-dropdown',
-  defaultValue: 'value2',
-}
-
-export const Disabled = Template.bind({})
-Disabled.args = {
-  id: 'input-dropdown',
-  name: 'input-dropdown',
-  disabled: true,
-}
-
-export const WithLabel = () => (
+export const withLabel = (): React.ReactElement => (
   <>
-    <Label htmlFor="input-dropdown">Dropdown label</Label>
+    <Label htmlFor="options">Dropdown label</Label>
     <Dropdown id="input-dropdown" name="input-dropdown">
-      {options}
+      <option>- Select - </option>
+      <option value="value1">Option A</option>
+      <option value="value2">Option B</option>
+      <option value="value3">Option C</option>
     </Dropdown>
   </>
+)
+
+export const disabled = (): React.ReactElement => (
+  <Dropdown id="input-dropdown" name="input-dropdown" disabled>
+    <option>- Select - </option>
+    <option value="value1">Option A</option>
+    <option value="value2">Option B</option>
+    <option value="value3">Option C</option>
+  </Dropdown>
 )
