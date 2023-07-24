027e03d4659d2491a5a03b025174f0f1eca6152d
Andrew Nelson <andy@andyhub.com>
2023-07-24 13:14:01 -0700

    updated address template

--- src/components/forms/Form/Form.stories.tsx
+++ src/components/forms/Form/Form.stories.tsx
@@ -114,85 +114,94 @@ export const nameForm = (): React.ReactElement => (
 export const addressForm = (): React.ReactElement => (
   <Form onSubmit={mockSubmit} large>
     <Fieldset legend="Mailing address" legendStyle="large">
-      <Label htmlFor="mailing-address-1">Street address 1</Label>
+      <p>
+        Required fields are marked with an asterisk (<abbr
+          title="required"
+          className="usa-hint usa-hint--required"
+          >*</abbr>).
+      </p>
+      <Label htmlFor="mailing-address-1">Street address</Label>
       <TextInput id="mailing-address-1" name="mailing-address-1" type="text" />
 
-      <Label htmlFor="mailing-address-2" hint=" (optional)">
-        Street address 2
+      <Label htmlFor="mailing-address-2">
+        Street address line 2
       </Label>
       <TextInput id="mailing-address-2" name="mailing-address-2" type="text" />
 
-      <div className="grid-row grid-gap">
-        <div className="mobile-lg:grid-col-8">
-          <Label htmlFor="city">City</Label>
-          <TextInput id="city" name="city" type="text" />
-        </div>
-        <div className="mobile-lg:grid-col-4">
-          <Label htmlFor="state">State</Label>
-          <Dropdown id="state" name="state">
-            <option>- Select -</option>
-            <option value="AL">Alabama</option>
-            <option value="AK">Alaska</option>
-            <option value="AZ">Arizona</option>
-            <option value="AR">Arkansas</option>
-            <option value="CA">California</option>
-            <option value="CO">Colorado</option>
-            <option value="CT">Connecticut</option>
-            <option value="DE">Delaware</option>
-            <option value="DC">District of Columbia</option>
-            <option value="FL">Florida</option>
-            <option value="GA">Georgia</option>
-            <option value="HI">Hawaii</option>
-            <option value="ID">Idaho</option>
-            <option value="IL">Illinois</option>
-            <option value="IN">Indiana</option>
-            <option value="IA">Iowa</option>
-            <option value="KS">Kansas</option>
-            <option value="KY">Kentucky</option>
-            <option value="LA">Louisiana</option>
-            <option value="ME">Maine</option>
-            <option value="MD">Maryland</option>
-            <option value="MA">Massachusetts</option>
-            <option value="MI">Michigan</option>
-            <option value="MN">Minnesota</option>
-            <option value="MS">Mississippi</option>
-            <option value="MO">Missouri</option>
-            <option value="MT">Montana</option>
-            <option value="NE">Nebraska</option>
-            <option value="NV">Nevada</option>
-            <option value="NH">New Hampshire</option>
-            <option value="NJ">New Jersey</option>
-            <option value="NM">New Mexico</option>
-            <option value="NY">New York</option>
-            <option value="NC">North Carolina</option>
-            <option value="ND">North Dakota</option>
-            <option value="OH">Ohio</option>
-            <option value="OK">Oklahoma</option>
-            <option value="OR">Oregon</option>
-            <option value="PA">Pennsylvania</option>
-            <option value="RI">Rhode Island</option>
-            <option value="SC">South Carolina</option>
-            <option value="SD">South Dakota</option>
-            <option value="TN">Tennessee</option>
-            <option value="TX">Texas</option>
-            <option value="UT">Utah</option>
-            <option value="VT">Vermont</option>
-            <option value="VA">Virginia</option>
-            <option value="WA">Washington</option>
-            <option value="WV">West Virginia</option>
-            <option value="WI">Wisconsin</option>
-            <option value="WY">Wyoming</option>
-            <option value="AA">AA - Armed Forces Americas</option>
-            <option value="AE">AE - Armed Forces Africa</option>
-            <option value="AE">AE - Armed Forces Canada</option>
-            <option value="AE">AE - Armed Forces Europe</option>
-            <option value="AE">AE - Armed Forces Middle East</option>
-            <option value="AP">AP - Armed Forces Pacific</option>
-          </Dropdown>
-        </div>
-      </div>
+      <Label htmlFor="city">City <abbr
+        title="required"
+        className="usa-hint usa-hint--required"
+        >*</abbr>
+      </Label>
+      <TextInput id="city" name="city" type="text" required />
 
-      <Label htmlFor="zip">ZIP</Label>
+      <Label htmlFor="state">State, territory, or military post <abbr
+        title="required"
+        className="usa-hint usa-hint--required"
+        >*</abbr>
+      </Label>
+      <Dropdown id="state" name="state" required>
+        <option>- Select -</option>
+        <option value="AL">Alabama</option>
+        <option value="AK">Alaska</option>
+        <option value="AZ">Arizona</option>
+        <option value="AR">Arkansas</option>
+        <option value="CA">California</option>
+        <option value="CO">Colorado</option>
+        <option value="CT">Connecticut</option>
+        <option value="DE">Delaware</option>
+        <option value="DC">District of Columbia</option>
+        <option value="FL">Florida</option>
+        <option value="GA">Georgia</option>
+        <option value="HI">Hawaii</option>
+        <option value="ID">Idaho</option>
+        <option value="IL">Illinois</option>
+        <option value="IN">Indiana</option>
+        <option value="IA">Iowa</option>
+        <option value="KS">Kansas</option>
+        <option value="KY">Kentucky</option>
+        <option value="LA">Louisiana</option>
+        <option value="ME">Maine</option>
+        <option value="MD">Maryland</option>
+        <option value="MA">Massachusetts</option>
+        <option value="MI">Michigan</option>
+        <option value="MN">Minnesota</option>
+        <option value="MS">Mississippi</option>
+        <option value="MO">Missouri</option>
+        <option value="MT">Montana</option>
+        <option value="NE">Nebraska</option>
+        <option value="NV">Nevada</option>
+        <option value="NH">New Hampshire</option>
+        <option value="NJ">New Jersey</option>
+        <option value="NM">New Mexico</option>
+        <option value="NY">New York</option>
+        <option value="NC">North Carolina</option>
+        <option value="ND">North Dakota</option>
+        <option value="OH">Ohio</option>
+        <option value="OK">Oklahoma</option>
+        <option value="OR">Oregon</option>
+        <option value="PA">Pennsylvania</option>
+        <option value="RI">Rhode Island</option>
+        <option value="SC">South Carolina</option>
+        <option value="SD">South Dakota</option>
+        <option value="TN">Tennessee</option>
+        <option value="TX">Texas</option>
+        <option value="UT">Utah</option>
+        <option value="VT">Vermont</option>
+        <option value="VA">Virginia</option>
+        <option value="WA">Washington</option>
+        <option value="WV">West Virginia</option>
+        <option value="WI">Wisconsin</option>
+        <option value="WY">Wyoming</option>
+        <option value="AA">AA - Armed Forces Americas</option>
+        <option value="AE">AE - Armed Forces Africa</option>
+        <option value="AE">AE - Armed Forces Canada</option>
+        <option value="AE">AE - Armed Forces Europe</option>
+        <option value="AE">AE - Armed Forces Middle East</option>
+        <option value="AP">AP - Armed Forces Pacific</option>
+      </Dropdown>
+
+      <Label htmlFor="zip">ZIP Code</Label>
       <TextInput
         id="zip"
         name="zip"
@@ -200,6 +209,9 @@ export const addressForm = (): React.ReactElement => (
         inputSize="medium"
         pattern="[\d]{5}(-[\d]{4})?"
       />
+
+      <Label htmlFor="urbanization">Urbanization (Puerto Rico only)</Label>
+      <TextInput id="urbanization" name="urbanization" type="text" />
     </Fieldset>
   </Form>
 )
