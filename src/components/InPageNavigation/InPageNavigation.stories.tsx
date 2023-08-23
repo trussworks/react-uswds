a631909a25d18e11f15fe5ee2bc701a321c41666
Andrew Nelson <andy@andyhub.com>
2023-08-23 14:35:20 -0700

    added story controls

--- src/components/InPageNavigation/InPageNavigation.stories.tsx
+++ src/components/InPageNavigation/InPageNavigation.stories.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import { InPageNavigation } from './InPageNavigation'
 import { CONTENT } from './content'
+import { HeadingLevel } from '../../types/headingLevel'
 
 export default {
   title: 'Components/In-Page Navigation',
   component: InPageNavigation,
+  argTypes: {
+    headingLevel: {
+      control: 'select',
+      options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+    },
+    rootMargin: {
+      control: 'text',
+    },
+    scrollOffset: {
+      control: 'text',
+    },
+    threshold: {
+      control: { type: 'range', min: 0, max: 1, step: 0.01 },
+    },
+    title: {
+      control: 'text',
+    },
+  },
+  args: {
+    headingLevel: 'h4',
+    rootMargin: '0px 0px 0px 0px',
+    scrollOffset: '0rem',
+    threshold: 1,
+    title: 'On this page',
+  },
   parameters: {
     docs: {
       description: {
@@ -18,17 +44,21 @@ Source: https://designsystem.digital.gov/components/in-page-navigation/
   },
 }
 
-export const Default = (): React.ReactElement => (
-  <InPageNavigation content={CONTENT} />
-)
+type StorybookArguments = {
+  headingLevel: HeadingLevel
+  rootMargin: string
+  scrollOffset: string
+  threshold: number
+  title: string
+}
 
-export const Offset = (): React.ReactElement => (
+export const Default = (argTypes: StorybookArguments): React.ReactElement => (
   <InPageNavigation
     content={CONTENT}
-    headingLevel="h4"
-    rootMargin="0px 0px 0px 0px"
-    scrollOffset="2rem"
-    threshold={1}
-    title="On this page"
+    headingLevel={argTypes.headingLevel}
+    rootMargin={argTypes.rootMargin}
+    scrollOffset={argTypes.scrollOffset}
+    threshold={argTypes.threshold}
+    title={argTypes.title}
   />
 )
