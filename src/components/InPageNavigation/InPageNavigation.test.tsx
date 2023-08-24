ade5417edd29120cad4611d216c85b3412d63d02
Andrew Nelson <andy@andyhub.com>
2023-08-24 12:05:48 -0700

    added some tests

--- src/components/InPageNavigation/InPageNavigation.test.tsx
+++ src/components/InPageNavigation/InPageNavigation.test.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
-import { screen, render } from '@testing-library/react'
+import { screen, render, getByTestId, getByRole } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { InPageNavigation } from './InPageNavigation'
+import { HeadingLevel } from '../../types/headingLevel'
 import { CONTENT } from './content'
+import { log } from 'console'
 
 describe('InPageNavigation component', () => {
+  const props = {
+    content: CONTENT,
+    headingLevel: 'h1' as HeadingLevel,
+    rootMargin: '1px 2px 3px 4px',
+    scrollOffset: '42px',
+    threshold: 0.75,
+    title: 'What do we have <i>here</i>?',
+  }
+
   const setup = () => {
-    const utils = render(<InPageNavigation content={CONTENT} />)
+    const utils = render(
+      <InPageNavigation
+        content={props.content}
+        headingLevel={props.headingLevel}
+        rootMargin={props.rootMargin}
+        scrollOffset={props.scrollOffset}
+        threshold={props.threshold}
+        title={props.title}
+      />
+    )
     const nav = screen.getByTestId('InPageNavigation')
     const user = userEvent.setup()
     return {
@@ -16,8 +36,51 @@ describe('InPageNavigation component', () => {
     }
   }
 
+  beforeEach(() => {
+    // IntersectionObserver isn't available in test environment
+    const mockIntersectionObserver = jest.fn()
+    mockIntersectionObserver.mockReturnValue({
+      observe: () => null,
+      unobserve: () => null,
+      disconnect: () => null,
+    })
+    window.IntersectionObserver = mockIntersectionObserver
+  })
+
   it('renders without errors', () => {
-    const { nav } = setup()
+    render(<InPageNavigation content={props.content} />)
+
+    const nav = screen.getByTestId('InPageNavigation')
     expect(nav).toBeInTheDocument()
+
+    const heading = getByRole(nav, 'heading', {
+      level: 4,
+      name: 'On this page',
+    })
+    expect(heading).toBeInTheDocument()
+    //TODO rootmargin
+    //TODO scrolloffset
+    //TODO threshold
+  })
+
+  it('sets the heading and title', () => {
+    const { nav } = setup()
+    const heading = getByRole(nav, 'heading', {
+      level: Number(props.headingLevel.slice(-1)),
+      name: props.title,
+    })
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('sets the root margin', () => {
+    const { nav } = setup()
+  })
+
+  it('sets the scroll offset', () => {
+    const { nav } = setup()
+  })
+
+  it('sets the threshold', () => {
+    const { nav } = setup()
   })
 })
