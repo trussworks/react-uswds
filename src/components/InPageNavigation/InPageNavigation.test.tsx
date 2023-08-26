ddabf58d8b9bfb78ecfe028c87db4c45a1d3ebf7
Andrew Nelson <andy@andyhub.com>
2023-08-26 05:48:54 +0200

    test cleanup

--- src/components/InPageNavigation/InPageNavigation.test.tsx
+++ src/components/InPageNavigation/InPageNavigation.test.tsx
@@ -1,32 +1,27 @@
 import React from 'react'
-import { screen, render, getByTestId, getByRole } from '@testing-library/react'
+import { screen, render, getByRole } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { InPageNavigation } from './InPageNavigation'
 import { HeadingLevel } from '../../types/headingLevel'
 import { CONTENT } from './content'
-import { log } from 'console'
 
 describe('InPageNavigation component', () => {
   const props = {
     content: CONTENT,
     headingLevel: 'h1' as HeadingLevel,
-    rootMargin: '1px 2px 3px 4px',
-    scrollOffset: '42px',
-    threshold: 0.75,
     title: 'What do we have <i>here</i>?',
   }
 
-  const setup = () => {
-    const utils = render(
-      <InPageNavigation
-        content={props.content}
-        headingLevel={props.headingLevel}
-        rootMargin={props.rootMargin}
-        scrollOffset={props.scrollOffset}
-        threshold={props.threshold}
-        title={props.title}
-      />
-    )
+  const setup = (plain?: boolean) => {
+    const utils = plain
+      ? render(<InPageNavigation content={props.content} />)
+      : render(
+          <InPageNavigation
+            content={props.content}
+            headingLevel={props.headingLevel}
+            title={props.title}
+          />
+        )
     const nav = screen.getByTestId('InPageNavigation')
     const user = userEvent.setup()
     return {
@@ -48,19 +43,13 @@ describe('InPageNavigation component', () => {
   })
 
   it('renders without errors', () => {
-    render(<InPageNavigation content={props.content} />)
-
-    const nav = screen.getByTestId('InPageNavigation')
+    const { nav } = setup(true)
     expect(nav).toBeInTheDocument()
-
     const heading = getByRole(nav, 'heading', {
       level: 4,
       name: 'On this page',
     })
     expect(heading).toBeInTheDocument()
-    //TODO rootmargin
-    //TODO scrolloffset
-    //TODO threshold
   })
 
   it('sets the heading and title', () => {
@@ -70,17 +59,5 @@ describe('InPageNavigation component', () => {
       name: props.title,
     })
     expect(heading).toBeInTheDocument()
-  })
-
-  it('sets the root margin', () => {
-    const { nav } = setup()
-  })
-
-  it('sets the scroll offset', () => {
-    const { nav } = setup()
-  })
-
-  it('sets the threshold', () => {
-    const { nav } = setup()
   })
 })
