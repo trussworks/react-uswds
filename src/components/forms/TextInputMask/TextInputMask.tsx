1f6025059f8249fa5a08892c8dbcf418c8e2163f
Nicholas Guyett <nicholas.guyett@coforma.io>
2023-09-06 09:50:42 -0700

    Add support for defaultValue and remove redundant value state

--- src/components/forms/TextInputMask/TextInputMask.tsx
+++ src/components/forms/TextInputMask/TextInputMask.tsx
@@ -8,6 +8,37 @@ export type AllProps = TextInputProps & {
   charset?: string
 }
 
+function maskString(value: string, mask: string, charset?: string) {
+  const maskData = charset || mask
+
+  const strippedValue = charset
+    ? value.replace(/\W/g, '')
+    : value.replace(/\D/g, '')
+  const charIsInteger = (v: string) => !Number.isNaN(parseInt(v, 10))
+  const charIsLetter = (v: string) => (v ? v.match(/[A-Z]/i) : false)
+  const maskedNumber = '_#dDmMyY9'
+  const maskedLetter = 'A'
+  let newValue = ''
+  for (let m = 0, v = 0; m < maskData.length; m++) {
+    const isInt = charIsInteger(strippedValue[v])
+    const isLet = charIsLetter(strippedValue[v])
+    const matchesNumber = maskedNumber.indexOf(maskData[m]) >= 0
+    const matchesLetter = maskedLetter.indexOf(maskData[m]) >= 0
+    if ((matchesNumber && isInt) || (charset && matchesLetter && isLet)) {
+      newValue += strippedValue[v++]
+    } else if (
+      strippedValue[v] === undefined || // if no characters left and the pattern is non-special character
+      (!charset && !isInt && matchesNumber) ||
+      (charset && ((matchesLetter && !isLet) || (matchesNumber && !isLet)))
+    ) {
+      break
+    } else {
+      newValue += maskData[m]
+    }
+  }
+
+  return newValue
+}
 
 export const TextInputMask = ({
   id,
@@ -25,42 +56,16 @@ export const TextInputMask = ({
     className
   )
 
-  const [inputValue, setInputValue] = useState(defaultValue)
-  const [maskValue, setMaskValue] = useState(mask)
-  const [iValue, setIValue] = useState('')
+  const [value, setValue] = useState(
+    // Ensure that this component preserves the expected behavior when a user sets the defaultValue
+    maskString((defaultValue as string) ?? ``, mask, charset)
+  )
+  const [maskValue, setMaskValue] = useState(mask.substring(value.length))
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const value = e.target.value
-    const maskData = charset || mask
-    if (undefined === maskData) return
-    const strippedValue = charset
-      ? value.replace(/\W/g, '')
-      : value.replace(/\D/g, '')
-    const charIsInteger = (v: string) => !Number.isNaN(parseInt(v, 10))
-    const charIsLetter = (v: string) => (v ? v.match(/[A-Z]/i) : false)
-    const maskedNumber = '_#dDmMyY9'
-    const maskedLetter = 'A'
-    let newValue = ''
-    for (let m = 0, v = 0; m < maskData.length; m++) {
-      const isInt = charIsInteger(strippedValue[v])
-      const isLet = charIsLetter(strippedValue[v])
-      const matchesNumber = maskedNumber.indexOf(maskData[m]) >= 0
-      const matchesLetter = maskedLetter.indexOf(maskData[m]) >= 0
-      if ((matchesNumber && isInt) || (charset && matchesLetter && isLet)) {
-        newValue += strippedValue[v++]
-      } else if (
-        strippedValue[v] === undefined || // if no characters left and the pattern is non-special character
-        (!charset && !isInt && matchesNumber) ||
-        (charset && ((matchesLetter && !isLet) || (matchesNumber && !isLet)))
-      ) {
-        break
-      } else {
-        newValue += maskData[m]
-      }
-    }
+    const newValue = maskString(e.target.value, mask, charset)
+
     setMaskValue(mask.substring(newValue.length))
-    setIValue(newValue)
-    setInputValue(newValue)
-    inputProps.onChange
+    setValue(newValue)
 
     onChange?.(e)
   }
@@ -71,7 +76,7 @@ export const TextInputMask = ({
         className="usa-input-mask--content"
         aria-hidden
         data-testid={`${id}Mask`}>
-        <i>{iValue}</i>
+        <i>{value}</i>
         {maskValue}
       </span>
       <TextInput
@@ -80,7 +85,7 @@ export const TextInputMask = ({
         id={id}
         maxLength={mask.length}
         onChange={handleChange}
-        value={inputValue}
+        value={value}
         {...inputProps}
       />
     </span>
