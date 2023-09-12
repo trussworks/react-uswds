38cbb7c692d57b078f26921e4b24c9220c1320b0
Roger Steve Ruiz <rogeruiz@truss.works>
2023-09-12 11:56:20 -0500

    Refactoring the iOS function;
    This was throwing all sorts of Typescript errors. I simplified the check a lot
    to only use user-agent string checking. It's not as robust, but it removes the
    TS errors from the project. Also, `navigator.platform` has been deprecated. From
    everything I searched for it seems like when this code was added in early 2021,
    the code was acceptable. But now in 2023, the community consensus is to only
    check the user-agent and avoid checking for `window.MSStream`.

--- src/components/forms/DatePicker/utils.tsx
+++ src/components/forms/DatePicker/utils.tsx
@@ -561,6 +561,4 @@ export const handleTabKey = (
 // iOS detection from: http://stackoverflow.com/a/9039885/177710
 export const isIosDevice = (): boolean =>
   typeof navigator !== 'undefined' &&
-  (navigator.userAgent.match(/(iPod|iPhone|iPad)/g) ||
-    (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) &&
-  !window.MSStream
+  !!navigator.userAgent.match(/ipad|ipod|iphone/i)
