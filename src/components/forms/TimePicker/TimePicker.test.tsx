bb6f439d321d73fc789057c0ecbbbbe8d428118d
Muhammad Adeel <87698014+m7adeel@users.noreply.github.com>
2023-10-18 23:59:17 +0500

    fix: Update TimePicker test file

--- src/components/forms/TimePicker/TimePicker.test.tsx
+++ src/components/forms/TimePicker/TimePicker.test.tsx
@@ -12,7 +12,7 @@ describe('TimePicker Component', () => {
   const testProps = {
     id: 'appointment-time',
     name: 'appointment-time',
-    label: 'Appointment Time',
+    label: 'Appointment time',
     onChange: jest.fn(),
   }
 
