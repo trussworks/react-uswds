889b9856543780517fc40d8b02dc6598817d573f
Chris Vincent <c.m.vincent@pm.me>
2023-11-14 09:23:08 -0700

    fix(a11y): add close icon alt text

--- src/components/header/NavCloseButton/NavCloseButton.tsx
+++ src/components/header/NavCloseButton/NavCloseButton.tsx
@@ -18,7 +18,7 @@ export const NavCloseButton = ({
       aria-label="Close Navigation Menu"
       {...buttonProps}
       type="button">
-      <Icon.Close size={3} aria-hidden="true" />
+      <Icon.Close size={3} aria-hidden="true" alt="Close Icon" />
     </button>
   )
 }
