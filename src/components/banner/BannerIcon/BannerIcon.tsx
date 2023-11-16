8efb5f7c37015811e1269a0da4cef98b0220e372
James Hancock <james.hancock@torchbox.com>
2023-11-16 15:48:24 +0000

    fix: Remove image role attribute from Banner Icon component
    Closes #2662

--- src/components/banner/BannerIcon/BannerIcon.tsx
+++ src/components/banner/BannerIcon/BannerIcon.tsx
@@ -10,11 +10,9 @@ export const BannerIcon = ({
   const classes = classNames('usa-banner__icon usa-media-block__img', className)
 
   return (
-    // eslint-disable-next-line jsx-a11y/no-redundant-roles
     <img
       className={classes}
       src={src}
-      role="img"
       alt={alt}
       aria-hidden="true"
       {...imgProps}
