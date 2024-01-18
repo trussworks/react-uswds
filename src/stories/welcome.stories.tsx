8f65ec461d14db1350fc11d24d2676367d5b0294
Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>
2024-01-18 12:12:20 -0600

    feat(storybook): storybook 7 (#2533)
    Co-authored-by: John Gedeon <john@gedeons.com>

--- src/stories/welcome.stories.tsx
+++ src/stories/welcome.stories.tsx
@@ -43,10 +43,7 @@ export const Welcome = (): React.ReactElement => {
       </div>
       <div className="display-flex flex-align-center flex-justify-center padding-top-4">
         <div className="margin-right-3" style={{ width: '150px' }}>
-          <img
-            src="https://raw.githubusercontent.com/uswds/uswds-site/main/img/uswds-logo/4c-lg-white.svg"
-            alt="USWDS logo"
-          />
+          <img src="/uswds.svg" alt="USWDS logo" />
         </div>
         <div>
           <h1 className="margin-y-0" style={{ maxWidth: '527px' }}>
