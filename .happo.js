03cdc59513ce5c9f9221266f56ee33555f375102
Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>
2024-02-02 19:11:19 -0600

    Revert "update happo config to esm syntax"
    This reverts commit 729df125f8a218b37ca98d0b3fc4716dcaa702cf.

--- .happo.js
+++ .happo.js
@@ -1,32 +1,33 @@
-import { RemoteBrowserTarget } from 'happo.io'
-import happoPluginStorybook from 'happo-plugin-storybook'
-import dotenv from 'dotenv';
+const { RemoteBrowserTarget } = require('happo.io')
+const happoPluginStorybook = require('happo-plugin-storybook')
 
-dotenv.config()
+require('dotenv').config()
 
-export const apiKey = process.env.HAPPO_API_KEY
-export const apiSecret = process.env.HAPPO_API_SECRET
-export const targets = {
-  chrome: new RemoteBrowserTarget('chrome', {
-    viewport: '1024x768',
-  }),
-  firefox: new RemoteBrowserTarget('firefox', {
-    viewport: '1024x768',
-  }),
-  edge: new RemoteBrowserTarget('edge', {
-    viewport: '1024x768',
-  }),
-  safari: new RemoteBrowserTarget('safari', {
-    viewport: '1024x768',
-  }),
-  'ios-safari': new RemoteBrowserTarget('ios-safari', {
-    viewport: '375x667',
-  }),
+module.exports = {
+  apiKey: process.env.HAPPO_API_KEY,
+  apiSecret: process.env.HAPPO_API_SECRET,
+  targets: {
+    chrome: new RemoteBrowserTarget('chrome', {
+      viewport: '1024x768',
+    }),
+    firefox: new RemoteBrowserTarget('firefox', {
+      viewport: '1024x768',
+    }),
+    edge: new RemoteBrowserTarget('edge', {
+      viewport: '1024x768',
+    }),
+    safari: new RemoteBrowserTarget('safari', {
+      viewport: '1024x768',
+    }),
+    'ios-safari': new RemoteBrowserTarget('ios-safari', {
+      viewport: '375x667',
+    }),
+  },
+  plugins: [
+    // see https://github.com/happo/happo-plugin-storybook for a list of options you can pass to the plugin
+    happoPluginStorybook({
+      outputDir: 'storybook-static',
+      usePrebuiltPackage: true,
+    }),
+  ],
 }
-export const plugins = [
-  // see https://github.com/happo/happo-plugin-storybook for a list of options you can pass to the plugin
-  happoPluginStorybook({
-    outputDir: 'storybook-static',
-    usePrebuiltPackage: true,
-  }),
-]
