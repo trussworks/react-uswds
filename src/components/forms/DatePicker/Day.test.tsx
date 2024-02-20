d4b30d8e87858dc1b8e5fa424c89aa14eb5309f4
Andrew Nelson <andy@andyhub.com>
2024-02-20 10:31:57 -0800

    fix: fixed lint warnings (#2774)

--- src/components/forms/DatePicker/Day.test.tsx
+++ src/components/forms/DatePicker/Day.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { fireEvent, render } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { userEvent } from '@testing-library/user-event'
 
 import { Day } from './Day'
 
