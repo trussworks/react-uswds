bcd1d150f1ec46574161133f8c404df4e0c5d9a4
Matt Mower <matt.mower@cornerstonenw.com>
2026-09-15 10:56:35 -0700

    Update initial focus element detection for modals
    When modal opens and initial focus is determined...
    1. continue respecting the element with `[data-focus]` if it exists,
       then fall back to...
    2. the first non-disabled button in the footer if it exists, then fall
       back to...
    3. any non-disabled button in the modal if it exists, and finally fall
       back to...
    4. the modal itself.
    
    Update type annotations, fixing incorrect type assertion on the queried
    elements. Update tests to account for the new logic.

--- src/components/modal/Modal.test.tsx
+++ src/components/modal/Modal.test.tsx
@@ -525,12 +525,8 @@ describe('Modal component', () => {
       it('activates a focus trap', async () => {
         const user = userEvent.setup()
 
-        const {
-          modalWrapper,
-          modalWindow,
-          queryForOpenButton,
-          queryForCloseButton,
-        } = renderWithModalRoot(<ExampleModal />)
+        const { modalWrapper, queryForOpenButton, queryForCloseButton } =
+          renderWithModalRoot(<ExampleModal />)
 
         const openButton = queryForOpenButton()
         expect(openButton).toBeInTheDocument()
@@ -541,14 +537,11 @@ describe('Modal component', () => {
 
         await waitFor(() => {
           expect(modalWrapper).toHaveClass('is-visible')
-          expect(modalWindow).toHaveFocus()
+          expect(
+            screen.getByRole('button', { name: 'Continue without saving' })
+          ).toHaveFocus()
           expect(closeButton).toBeInTheDocument()
         })
-
-        await user.tab()
-        expect(
-          screen.getByRole('button', { name: 'Continue without saving' })
-        ).toHaveFocus()
 
         await user.tab()
         expect(screen.getByRole('button', { name: 'Go back' })).toHaveFocus()
@@ -566,42 +559,7 @@ describe('Modal component', () => {
       it('returns focus to the opener element on close', async () => {
         const user = userEvent.setup()
 
-        const {
-          modalWrapper,
-          modalWindow,
-          queryForOpenButton,
-          queryForCloseButton,
-        } = renderWithModalRoot(<ExampleModal />)
-
-        const openButton = queryForOpenButton()
-        expect(openButton).toBeInTheDocument()
-
-        await user.click(openButton!)
-
-        await waitFor(() => {
-          expect(modalWrapper).toHaveClass('is-visible')
-          expect(modalWindow).toHaveFocus()
-        })
-
-        const closeButton = queryForCloseButton()
-        expect(closeButton).toBeInTheDocument()
-
-        await user.tab()
-        expect(
-          screen.getByRole('button', {
-            name: 'Continue without saving',
-          })
-        ).toHaveFocus()
-
-        await user.click(closeButton!)
-
-        expect(openButton).toHaveFocus()
-      })
-
-      it('the escape key closes the modal', async () => {
-        const user = userEvent.setup()
-
-        const { modalWrapper, modalWindow, queryForOpenButton } =
+        const { modalWrapper, queryForOpenButton, queryForCloseButton } =
           renderWithModalRoot(<ExampleModal />)
 
         const openButton = queryForOpenButton()
@@ -611,7 +569,39 @@ describe('Modal component', () => {
 
         await waitFor(() => {
           expect(modalWrapper).toHaveClass('is-visible')
-          expect(modalWindow).toHaveFocus()
+          expect(
+            screen.getByRole('button', { name: 'Continue without saving' })
+          ).toHaveFocus()
+        })
+
+        const closeButton = queryForCloseButton()
+        expect(closeButton).toBeInTheDocument()
+
+        await user.tab()
+        expect(screen.getByRole('button', { name: 'Go back' })).toHaveFocus()
+
+        await user.click(closeButton!)
+
+        expect(openButton).toHaveFocus()
+      })
+
+      it('the escape key closes the modal', async () => {
+        const user = userEvent.setup()
+
+        const { modalWrapper, queryForOpenButton } = renderWithModalRoot(
+          <ExampleModal />
+        )
+
+        const openButton = queryForOpenButton()
+        expect(openButton).toBeInTheDocument()
+
+        await user.click(openButton!)
+
+        await waitFor(() => {
+          expect(modalWrapper).toHaveClass('is-visible')
+          expect(
+            screen.getByRole('button', { name: 'Continue without saving' })
+          ).toHaveFocus()
         })
 
         await user.keyboard('{escape}')
@@ -639,6 +629,25 @@ describe('Modal component', () => {
           expect(
             screen.getByRole('button', { name: 'Focus me first' })
           ).toHaveFocus()
+        })
+      })
+
+      it('focuses the close button when no other buttons', async () => {
+        const modalRef = createRef<ModalRef>()
+        const handleOpen = () => modalRef.current?.toggleModal(undefined, true)
+
+        const { modalWrapper, queryForCloseButton } = renderWithModalRoot(
+          <Modal id="testModal" ref={modalRef} modalRoot="#modal-root">
+            <p>Test modal</p>
+            <a href="#test">Focus target</a>
+          </Modal>
+        )
+
+        handleOpen()
+
+        await waitFor(() => {
+          expect(modalWrapper).toHaveClass('is-visible')
+          expect(queryForCloseButton()).toHaveFocus()
         })
       })
     })
@@ -702,8 +711,9 @@ describe('Modal component', () => {
       it('the escape key does not close the modal', async () => {
         const user = userEvent.setup()
 
-        const { modalWrapper, modalWindow, queryForOpenButton } =
-          renderWithModalRoot(<ExampleModal forceAction />)
+        const { modalWrapper, queryForOpenButton } = renderWithModalRoot(
+          <ExampleModal forceAction />
+        )
 
         const openButton = queryForOpenButton()
         expect(openButton).toBeInTheDocument()
@@ -712,14 +722,18 @@ describe('Modal component', () => {
 
         await waitFor(() => {
           expect(modalWrapper).toHaveClass('is-visible')
-          expect(modalWindow).toHaveFocus()
+          expect(
+            screen.getByRole('button', { name: 'Continue without saving' })
+          ).toHaveFocus()
         })
 
         await user.keyboard('{escape}')
 
         await waitFor(() => {
           expect(modalWrapper).toHaveClass('is-visible')
-          expect(modalWindow).toHaveFocus()
+          expect(
+            screen.getByRole('button', { name: 'Continue without saving' })
+          ).toHaveFocus()
         })
       })
     })
